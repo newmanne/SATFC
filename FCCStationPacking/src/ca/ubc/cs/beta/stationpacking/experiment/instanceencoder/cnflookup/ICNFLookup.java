dfaca99c68b346025446f8a2db8e5ebf91e389af
Nick Arnosti <narnosti@rescomp-11-219594.stanford.edu>
2013-04-11 00:47:34 -0700

    My various code updates, described in my report. Now that I'm familiar with the project, I will be more diligent about updating frequently and about writing informative commit messages.
    Former-commit-id: cf976fd84f40a0c018df910525b8af3c08f74a6f [formerly b22d12afeb53d280b7296de154f519e686b4edf5]
    
    Former-commit-id: f535445026b7e04c7732c9c8db296a08b3024539

--- FCCStationPacking/src/ca/ubc/cs/beta/stationpacking/experiment/instanceencoder/cnflookup/ICNFLookup.java
+++ FCCStationPacking/src/ca/ubc/cs/beta/stationpacking/experiment/instanceencoder/cnflookup/ICNFLookup.java
@@ -14,31 +14,33 @@ import ca.ubc.cs.beta.stationpacking.data.Station;
  */
 public interface ICNFLookup {
 	
+	//NA - why are these all public? Which ones are actually called?
+	
 	/**
 	 * @param aStations - a set of stations.
 	 * @return True if it contains a CNF for given station set.
 	 */
-	public boolean hasCNFfor(Set<Station> aStations);
+	public boolean hasCNFfor(Set<Station> aStations, Integer ... aRange);
 	
 	/**
 	 * @param aStations - a set of stations.
 	 * @return CNF instance file name corresponding to input station set, if any.
 	 * @throws Exception - if input station set has no corresponding CNF name.
 	 */
-	public String getCNFfor(Set<Station> aStations) throws Exception;
+	public String getCNFfor(Set<Station> aStations, Integer ... aRange) throws Exception;
 	
 	/**
 	 * @param aStations - a set of stations.
 	 * @return The name it would attribute to a CNF corresponding to input station set.
 	 */
-	public String getCNFNamefor(Set<Station> aStations);
-	
+	//public String getCNFNamefor(Set<Station> aStations,Integer ... aRange);
+	//NA - commented out because it's only called internally
 	/**
 	 * Associates input set of stations to automatically generated CNF file name (see getCNFNamefor() method).
 	 * @param aStations - a set of stations.
 	 * @return The CNF name attributed to input station set.
 	 * @throws Exception
 	 */
-	public String addCNFfor(Set<Station> aStations) throws Exception;
+	public String addCNFfor(Set<Station> aStations, Integer ... aRange) throws Exception;
 
 }
