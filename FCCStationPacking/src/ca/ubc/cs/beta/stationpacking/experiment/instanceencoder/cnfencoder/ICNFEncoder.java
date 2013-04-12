94aee5ff6dc63f9acabc92e3872f1619efa1b4b7
Nick Arnosti <narnosti@dn5213ov.sunet>
2013-04-12 16:53:04 -0700

    NA - made changes to ICNFEncoder and the classes implementing it
    Former-commit-id: 5c30e67747fba3275ba12233eb8f435290f37589 [formerly c8762f4fabb475f11870de3c3f23c11c5931efa0]
    
    Former-commit-id: 4e2f2577a51d02985c0f2442372af6eec451feea

--- FCCStationPacking/src/ca/ubc/cs/beta/stationpacking/experiment/instanceencoder/cnfencoder/ICNFEncoder.java
+++ FCCStationPacking/src/ca/ubc/cs/beta/stationpacking/experiment/instanceencoder/cnfencoder/ICNFEncoder.java
@@ -1,8 +1,11 @@
 package ca.ubc.cs.beta.stationpacking.experiment.instanceencoder.cnfencoder;
 
+import java.util.Map;
 import java.util.Set;
 
 import ca.ubc.cs.beta.stationpacking.data.Station;
+import ca.ubc.cs.beta.stationpacking.data.manager.IConstraintManager;
+import ca.ubc.cs.beta.stationpacking.experiment.instance.IInstance;
 
 
 /**
@@ -18,8 +21,17 @@ public interface ICNFEncoder {
 	 * @param aStations - a set of stations to encode.
 	 * @return A DAC CNF encoding for that set of stations and encoder's relevant constraints. 
 	 */
-	public String encode(Set<Station> aStations);
 	
-	public boolean write(Set<Station> aStations,String aFileName);
+	//NA - the CNFEncoder should store station constraints internally
+	
+	//NA - Returns a String corresponding to the DIMACS cnf format of the problem
+	public String encode(IInstance aInstance, IConstraintManager aConstraintManager);
+	
+	//public boolean writeCNF(Set<Station> aStations, Set<Integer> aChannels, String aFileName);
+	
+	
+	public Map<Station,Integer> decode(IInstance aInstance, String aCNFAssignment);
+	
+	
 	
 }
