03fa4dfa744eb0b628e8be743ede11ccd8803331
Nick Arnosti <narnosti@rescomp-11-219594.stanford.edu>
2013-04-13 13:48:33 -0700

    Complete implementation of NickCNFEncoder.
    Former-commit-id: 6750652191fe8cf8ce68a9746306438f9b9cbfde

--- FCCStationPacking/src/ca/ubc/cs/beta/stationpacking/data/Station.java
+++ FCCStationPacking/src/ca/ubc/cs/beta/stationpacking/data/Station.java
@@ -1,6 +1,7 @@
 package ca.ubc.cs.beta.stationpacking.data;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Set;
@@ -11,20 +12,41 @@ import java.util.Set;
  * @author afrechet
  *
  */
-public class Station {
+public class Station implements Comparable<Station>{
 	
-	private Integer fID;
-	private Integer fPop; //NA - added optional population field
+	private final Integer fID;
+	private final Integer fPop;
+	private final Set<Integer> fDomain;
 	
 	public Station(Integer aID)
 	{
 		fID = aID;
 		fPop = -1;
+		fDomain = new HashSet<Integer>();
+		try{ 
+			throw new Exception("No Domain given for station "+fID);
+		} catch(Exception e){
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
 	}
 	
 	public Station(Integer aID,Integer aPop){
 		fID = aID;
 		fPop = aPop;
+		fDomain = new HashSet<Integer>();
+		try{ 
+			throw new Exception("No Domain given for station "+fID);
+		} catch(Exception e){
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	public Station(Integer aID,Integer aPop, Set<Integer> aDomain){
+		fID = aID;
+		fPop = aPop;
+		fDomain = aDomain;
 	}
 	
 	public int getID()
@@ -40,6 +62,10 @@ public class Station {
 	public String toString()
 	{
 		return "Station "+fID;
+	}
+	
+	public Set<Integer> getDomain(){
+		return fDomain;
 	}
 	
 	@Override
@@ -77,6 +103,11 @@ public class Station {
 			}
 		}
 		return aResult;	
+	}
+
+	@Override
+	public int compareTo(Station o) {
+		return fID - o.getID();
 	}
 
 }
