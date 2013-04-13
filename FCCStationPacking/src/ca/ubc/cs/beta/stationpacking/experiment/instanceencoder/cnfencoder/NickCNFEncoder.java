600382ea1efe7e02056894e8b6f4daef016eea07
Nick Arnosti <narnosti@rescomp-11-219594.stanford.edu>
2013-04-13 13:48:33 -0700

    Complete implementation of NickCNFEncoder.
    Former-commit-id: f3cb28b49828340c673aad298f1710e3249f0093 [formerly 6750652191fe8cf8ce68a9746306438f9b9cbfde]
    
    Former-commit-id: fdebe721da8cf71415356eba0ea0f04d54258e6c

--- FCCStationPacking/src/ca/ubc/cs/beta/stationpacking/experiment/instanceencoder/cnfencoder/NickCNFEncoder.java
+++ FCCStationPacking/src/ca/ubc/cs/beta/stationpacking/experiment/instanceencoder/cnfencoder/NickCNFEncoder.java
@@ -1,12 +1,13 @@
 package ca.ubc.cs.beta.stationpacking.experiment.instanceencoder.cnfencoder;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.HashSet;
-import java.util.TreeSet;
-import java.io.*;
+import java.util.List;
 
 //import ca.ubc.cs.beta.stationpacking.data.Constraint;
 import ca.ubc.cs.beta.stationpacking.data.Station;
@@ -17,80 +18,182 @@ import ca.ubc.cs.beta.stationpacking.experiment.instanceencoder.cnfencoder.ICNFE
 
 public class NickCNFEncoder implements ICNFEncoder {
 	
+	//Whenever a trivially UNSAT problem is detected, return this string
 	String fUNSAT_CNF = "p cnf 1 1\n 1 -1 0\n";
 
 	@Override
+	/* NA - takes an Instance and a set of Constraints, and returns
+	 * the DIMACS CNF corresponding to this instance of SAT.
+	 */
 	public String encode(IInstance aInstance, IConstraintManager aConstraintManager) {
-		Set<Integer> aChannels = aInstance.getChannelRange();
-		Set<Station> aStations = aInstance.getStations();
-		int numClauses = 0;
-		int numChannels = aChannels.size();
+		Map<Station,Integer> aInternalStationIDs = getInternalIDs(aInstance.getStations());
+		Map<Integer,Integer> aInternalChannelIDs = getInternalIDs(aInstance.getChannelRange());	
 		StringBuilder aBuilder = new StringBuilder();
-		//Sort the stations
-		TreeSet<Station> Stations = new TreeSet<Station>();
-		for(Station aStation : aStations) Stations.add(aStation);
-		//Sort the channels
-		TreeSet<Integer> Channels = new TreeSet<Integer>();
-		for(Integer aChannel : aChannels) Channels.add(aChannel);
-		//For each station, add corresponding constraints to string
-		Set<Station> aInterferingStations;
+		int aNumClauses = 0;
+		Set<Integer> aDomainInternal;
+		Set<Integer> aCOInterferingInternal;
+		Set<Integer> aADJInterferingInternal;
+		//For each station, add CNF clauses 
+		for(Station aStation : aInternalStationIDs.keySet()){
+			Integer aInternalID = aInternalStationIDs.get(aStation);
+			//Encode that aStation can be assigned to at most one channel
+			aNumClauses += writeBaseClauses(aInternalID,aInternalChannelIDs.values(),aBuilder);
+			//Encode that aStation must be assigned to at least one channel
+			aDomainInternal = mapSet(aInternalChannelIDs,aStation.getDomain());
+			if(aDomainInternal.isEmpty()) return fUNSAT_CNF;
+			else aNumClauses += writeDomainClauses(aInternalID,aDomainInternal,aInternalChannelIDs.size(),aBuilder);
+			//Encode that aStation cannot share a channel with any station in aConstraintManager.getCOInterferingStations(aStation)
+			aCOInterferingInternal = mapSet(aInternalStationIDs,aConstraintManager.getCOInterferingStations(aStation));
+			aNumClauses += writeConstraints("CO",aInternalID,aCOInterferingInternal,aInternalChannelIDs,aBuilder);
+			//Encode that aStation cannot be one channel below any station in aConstraintManager.getADJplusInterferingStations(aStation)
+			aADJInterferingInternal = mapSet(aInternalStationIDs,aConstraintManager.getADJplusInterferingStations(aStation));
+			aNumClauses += writeConstraints("ADJ",aInternalID,aADJInterferingInternal,aInternalChannelIDs,aBuilder);
+		}
+		aBuilder.insert(0,"p cnf "+aInternalStationIDs.size()*aInternalChannelIDs.size()+" "+aNumClauses+"\n");
+		return aBuilder.toString();
+	}
+	
+	
+	/* NA - takes an Instance and a string corresponding to a satisfying variable assignment.
+	 * Checks that each station is assigned exactly one channel, and that this channel is in its domain.
+	 * If these conditions are not met, it throws and catches an exception describing the problem.
+	 */
+	public Map<Station,Integer> decode(IInstance aInstance, String aCNFAssignment){
+		Map<Station,Integer> aInternalStationIDs = getInternalIDs(aInstance.getStations());
+		Map<Integer,Integer> aInternalChannelIDs = getInternalIDs(aInstance.getChannelRange());
+		Map<Station,Integer> aStationAssignment = new HashMap<Station,Integer>();
+		try{
+			Map<Integer,Boolean> aCNFdecoding = stringToAssignment(aCNFAssignment);
+			int aNumCNFVars = aCNFdecoding.size();
+			int aNumChannels = aInternalChannelIDs.size();
+			int aExpectedNumCNFVars = aInternalStationIDs.size()*aNumChannels;
+			if(aNumCNFVars == aExpectedNumCNFVars){
+				for(Station aStation : aInternalStationIDs.keySet()){
+					for(Integer aChannel : aInternalChannelIDs.keySet()){
+						Integer aVar = get_variable(aInternalStationIDs.get(aStation),aInternalChannelIDs.get(aChannel),aNumChannels);
+						if(aCNFdecoding.get(aVar)) {
+							if(aStationAssignment.put(aStation,aChannel)!=null){
+								throw new Exception(aStation+" assigned to multiple channels.");
+							}
+						}	
+					}
+					Integer aChannel = aStationAssignment.get(aStation);
+					if(aChannel==null) {
+						throw new Exception(aStation+" not assigned to a channel.");
+					}
+					else if(! aStation.getDomain().contains(aChannel)){
+						throw new Exception(aStation+"assigned channel "+aChannel+", which is not in its domain.");
+					}
+				}
+			} else throw new Exception(	"CNF Assignment includes "+aNumCNFVars+" variables, but encoding this instance requires "
+										+aExpectedNumCNFVars+" variables.");
+		} catch(Exception e) {
+			aStationAssignment.clear();
+			e.printStackTrace();
+		}
+		return aStationAssignment;
+	}
+	
+	/* NA - writes clasues of the form !x1 ^ !x2 for variables xi corresponding to the assignment of the
+	 * station with InternalID aInternalStationID to channels with InternalID in aInternalChannelIDs.
+	 */
+	private int writeBaseClauses(Integer aInternalStationID, Collection<Integer> aInternalChannelIDs, StringBuilder aBuilder){
+		int aNumWrittenClauses = 0;
+		Integer numChannels = aInternalChannelIDs.size();
 		final Set<Integer> aEmpty = new HashSet<Integer>();
 		Set<Integer> aNegatedVars = new HashSet<Integer>();
-		for(Station aStation : Stations){
-			//Write BASE clauses corresponding to the station domain
-			for(int j = 0; j < numChannels; j++){
-				for(int k = j+1; k < numChannels; k++){
+		for(Integer aInternalChannel1 : aInternalChannelIDs){
+			for(Integer aInternalChannel2 : aInternalChannelIDs){
+				if(aInternalChannel1 < aInternalChannel2){
 					aNegatedVars.clear();
-					aNegatedVars.add(get_variable(Stations.headSet(aStation).size(),j,numChannels));
-					aNegatedVars.add(get_variable(Stations.headSet(aStation).size(),k,numChannels));
-					numClauses += writeClause(aEmpty, aNegatedVars,aBuilder);
+					aNegatedVars.add(get_variable(aInternalStationID,aInternalChannel1,numChannels));
+					aNegatedVars.add(get_variable(aInternalStationID,aInternalChannel2,numChannels));
+					aNumWrittenClauses += writeClause(aEmpty, aNegatedVars, aBuilder);
 				}
 			}
-			//get domain of aStation, write corresponding base clause
-			Set<Integer> aVars = new HashSet<Integer>();
-			Set<Integer> aChannelDomain = aStation.getDomain();
-			for(Integer aChannel : aChannelDomain){
-				if(Channels.contains(aChannel)) {
-					aVars.add(get_variable(Stations.headSet(aStation).size(),Channels.headSet(aChannel).size(),numChannels));
-				}
-				if(aVars.isEmpty()) return fUNSAT_CNF;
-				else numClauses += writeClause(aVars,aEmpty,aBuilder);
-			}
-			//get COInterferingStations, write a clause for each one in Stations (don't duplicate)
-			aInterferingStations = aConstraintManager.getCOInterferingStations(aStation); //NA
-			for(Station aStation2 : aInterferingStations){
-				if(aStation.getID()<aStation2.getID() && Stations.contains(aStation2)){
-					for(int j = 0; j < numChannels; j++){
+		}
+		return aNumWrittenClauses;
+	}
+	
+	/* NA - writes a clause of the form x1 ^ x2 ^ ... ^ xn where the xi represent assigning the
+	 * station with InternalID aInternalStationID to one of the channels in its domain.
+	 */
+	private int writeDomainClauses(Integer aInternalStationID,Set<Integer> aInternalDomain, int aNumChannels, StringBuilder aBuilder){
+		int aNumWrittenClauses = 0;
+		Set<Integer> aVars = new HashSet<Integer>();
+		for(Integer aInternalChannel : aInternalDomain){
+			aVars.add(get_variable(aInternalStationID,aInternalChannel,aNumChannels));
+		}
+		aNumWrittenClauses += writeClause(aVars,new HashSet<Integer>(),aBuilder);
+		return aNumWrittenClauses;
+	}
+	
+	/* NA - writes clauses corresponding to constraints involving the station with internal ID aInternalStationID
+	 * and the set of stations represented by aInterferingInternal. The type of constraint (co-channel or adjacent channel)
+	 * is specified in the first argument, and the range of internal channel IDs is specified in the fourth parameter.
+	 */
+	private int writeConstraints(	String aConstraintType, Integer aInternalStationID, Set<Integer> aInterferingInternal,
+									Map<Integer,Integer> aInternalChannelIDs, StringBuilder aBuilder){
+		int aNumWrittenClauses = 0;
+		int aNumChannels = aInternalChannelIDs.size();
+		final Set<Integer> aEmpty = new HashSet<Integer>();
+		Set<Integer> aNegatedVars = new HashSet<Integer>();
+		if(aConstraintType.equals("CO")){
+			Collection<Integer> aInternalChannels = aInternalChannelIDs.values();
+			for(Integer aInternalStationID2 : aInterferingInternal){
+				if(aInternalStationID2 < aInternalStationID){
+					for(Integer aInternalChannel : aInternalChannels){
 						aNegatedVars.clear();
-						aNegatedVars.add(get_variable(Stations.headSet(aStation).size(),j,numChannels));
-						aNegatedVars.add(get_variable(Stations.headSet(aStation2).size(),j,numChannels));
-						numClauses += writeClause(aEmpty, aNegatedVars,aBuilder);
+						aNegatedVars.add(get_variable(aInternalStationID,aInternalChannel,aNumChannels));
+						aNegatedVars.add(get_variable(aInternalStationID2,aInternalChannel,aNumChannels));
+						aNumWrittenClauses += writeClause(aEmpty, aNegatedVars,aBuilder);
 					}
 				}
 			}
-			//get ADJplusInterferingStations, write a clause for each one in Stations
-			aInterferingStations = aConstraintManager.getADJplusInterferingStations(aStation);
-			for(Station aStation2 : aInterferingStations){
-				if(Stations.contains(aStation2)){
-					for(Integer aChannel : Channels){
-						if(Channels.higher(aChannel)==aChannel+1){
-							aNegatedVars.clear();
-							aNegatedVars.add(get_variable(Stations.headSet(aStation).size(),Channels.headSet(aChannel).size(),numChannels));
-							aNegatedVars.add(get_variable(Stations.headSet(aStation2).size(),Channels.headSet(aChannel).size()+1,numChannels));
-							numClauses += writeClause(aEmpty, aNegatedVars,aBuilder);
-						}
+		} else if(aConstraintType.equals("ADJ")){
+			for(Integer aInternalStationID2 : aInterferingInternal){
+				for(Integer aChannel : aInternalChannelIDs.keySet()){
+					if(aInternalChannelIDs.containsKey(aChannel+1)){
+						aNegatedVars.clear();
+						aNegatedVars.add(get_variable(aInternalStationID,aInternalChannelIDs.get(aChannel),aNumChannels));
+						aNegatedVars.add(get_variable(aInternalStationID2,aInternalChannelIDs.get(aChannel+1),aNumChannels));
+						aNumWrittenClauses += writeClause(aEmpty, aNegatedVars,aBuilder);
 					}
 				}
 			}	
+		} else try {
+			throw new Exception("Constraint Type must be either 'CO' or 'ADJ'.");
+		} catch(Exception e){
+			e.printStackTrace();
 		}
-		return "p cnf "+Stations.size()*Channels.size()+" "+numClauses+"\n"+aBuilder.toString();
+		return aNumWrittenClauses;
 	}
 	
-	//NA - Currently assigns each station a variable for each channel
+	//NA - Takes the string output of a SAT solver, turns it into the corresponding variable assignment.
+	private Map<Integer,Boolean> stringToAssignment(String aCNFAssignment) throws Exception{
+		Map<Integer,Boolean> aAssignment = new HashMap<Integer,Boolean>();
+		String[] a = aCNFAssignment.substring(aCNFAssignment.indexOf(" v "),aCNFAssignment.indexOf("CPU")).split(" |\n");
+		for(int i = 0; i < a.length; i++){
+			if(!(a[i].equals("v")||a[i].isEmpty())){
+				if(a[i].startsWith("-")) {
+					if(aAssignment.put(Integer.parseInt(a[i].substring(1)),false)!=null)
+						throw new Exception("Variable assigned to multiple truth values.");
+				} else 
+					if(aAssignment.put(Integer.parseInt(a[i]),true)!=null)
+						throw new Exception("Variable assigned to multiple truth values.");
+			}
+		}
+		if(aAssignment.remove(0)==null) throw new Exception("No terminating 0 in CNF string.");
+		return aAssignment;
+	}
+	
+	// NA - given a (Station,Channel) pair, returns the corresponding variable number
 	private Integer get_variable(int aStationNumber, int aChannelNumber, int numChannels){
 		return aStationNumber*numChannels+aChannelNumber+1;
+		
 	}
-	
+
+	// NA - given a set of variables and negated variables, writes a corresponding clause
 	private int writeClause(Set<Integer> aVars, Set<Integer> aNegatedVars, StringBuilder aBuilder){
 		for(Integer aVar : aVars){ aBuilder.append(aVar+" "); }
 		for(Integer aVar : aNegatedVars){ aBuilder.append("-"+aVar+" "); }
@@ -98,26 +201,26 @@ public class NickCNFEncoder implements ICNFEncoder {
 		return 1;
 	}
 	
-	public Map<Station,Integer> decode(IInstance aInstance, String aCNFAssignment){
-		Set<Integer> aChannels = aInstance.getChannelRange();
-		Set<Station> aStations = aInstance.getStations();
-		int numChannels = aChannels.size();
-		//Sort the stations
-		TreeSet<Station> Stations = new TreeSet<Station>();
-		for(Station aStation : aStations) Stations.add(aStation);
-		//Sort the channels
-		TreeSet<Integer> Channels = new TreeSet<Integer>();
-		for(Integer aChannel : aChannels) Channels.add(aChannel);
-		
-		//Figure out variable assignment and ensure that no constraints are violated
-		try{
-			throw new Exception("Method decode not implemented for class NickCNFEncoder.");
-		} catch(Exception e){
-			System.out.println(e.getMessage());
-			e.printStackTrace();
+	//NA - returns the result of applying aMap to the set aInputSet
+	private <T> Set<Integer> mapSet(Map<T,Integer> aMap, Set<T> aInputSet){
+		Integer aOutput;
+		Set<Integer> aOutputSet = new HashSet<Integer>();
+		for(T aT : aInputSet){
+			aOutput = aMap.get(aT);
+			if(aOutput!=null) aOutputSet.add(aOutput);
 		}
-		return new HashMap<Station,Integer>();
+		return aOutputSet;
 	}
-
+	
+	//NA - returns a map from items of type T to internal ID numbers used to represent them.
+	private <T extends Comparable<T>> Map<T,Integer> getInternalIDs(Set<T> aSet){
+		List<T> aSortedSet = new ArrayList<T>(aSet);
+		Collections.sort(aSortedSet);
+		Map<T,Integer> aInternalID = new HashMap<T,Integer>();
+		for(int i = 0; i < aSortedSet.size(); i++){
+			aInternalID.put(aSortedSet.get(i),i);
+		}
+		return aInternalID;
+	}
 
 }
