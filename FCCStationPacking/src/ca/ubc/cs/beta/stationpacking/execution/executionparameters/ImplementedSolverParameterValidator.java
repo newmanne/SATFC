d71b5318f73bbb049e39f8ec4c44d952dc546394
afrechet <afrechet@cs.ubc.ca>
2013-04-25 17:14:32 +0200

    - Added classes and files to commit.
    Former-commit-id: 2c13ae2607802459cbe644b4dfe0ca6430887fae [formerly 0f5c2631a4a7594055c2fec417e0509249aa39fe]
    
    Former-commit-id: 1955d11dd21e932a06beae2a8d83d4a19616beb8

---
+++ FCCStationPacking/src/ca/ubc/cs/beta/stationpacking/execution/executionparameters/ImplementedSolverParameterValidator.java
@@ -0,0 +1,28 @@
+package ca.ubc.cs.beta.stationpacking.execution.executionparameters;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.beust.jcommander.IParameterValidator;
+import com.beust.jcommander.ParameterException;
+
+public class ImplementedSolverParameterValidator implements IParameterValidator {
+	private Set<String> fImplementedSolvers = new HashSet<String>(Arrays.asList("picosat","clasp","glucose","plingeling","tunedclasp"));
+	
+	@Override
+	public void validate(String name, String value)
+			throws ParameterException {
+		if(name.equals("-SOLVER"))
+		{
+			if(!fImplementedSolvers.contains(value))
+			{
+				throw new ParameterException("Provided SOLVER parameter "+value+" is not an implemented solver.");
+			}
+		}
+		else
+		{
+			throw new ParameterException("ImplementedSolver verifier is only valid for the SOLVER parameter.");
+		}	
+	}
+}
