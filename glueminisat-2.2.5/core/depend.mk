45c6ac8c876de2d6595a6839e71d04e93ba6ec7d
Nick Arnosti <narnosti@host47-214.wifi.ubc.ca>
2013-05-31 13:10:53 -0700

    adding glueminisat and jna files
    Former-commit-id: 71dd663a62b72f49e67641496e32691f3776e34b [formerly d8788e0c75b3a257ddf3fcc90a1b5559f6eb8199]
    
    Former-commit-id: 4fc155aecc4c9638963f0d229d740f8f455e2af0

---
+++ glueminisat-2.2.5/core/depend.mk
@@ -0,0 +1,29 @@
+/Users/narnosti/Downloads/glueminisat-2.2.5/core/Main.o /Users/narnosti/Downloads/glueminisat-2.2.5/core/Main.or /Users/narnosti/Downloads/glueminisat-2.2.5/core/Main.od /Users/narnosti/Downloads/glueminisat-2.2.5/core/Main.op: /Users/narnosti/Downloads/glueminisat-2.2.5/core/Main.cc \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/utils/System.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/IntTypes.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/utils/ParseUtils.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/utils/Options.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Vec.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/XAlloc.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/core/Dimacs.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/core/SolverTypes.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Alg.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Map.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Alloc.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Heap.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/core/RecentVals.h
+/Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.o /Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.or /Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.od /Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.op: /Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.cc \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Sort.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Vec.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/IntTypes.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/XAlloc.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Heap.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Alg.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/utils/Options.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/utils/ParseUtils.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/core/SolverTypes.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Map.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Alloc.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/core/RecentVals.h
