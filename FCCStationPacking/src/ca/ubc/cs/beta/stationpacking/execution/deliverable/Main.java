cf93e2c6fbd0d52f76034747f3b2e1e3badfc2ce
Nick Arnosti <narnosti@host47-214.wifi.ubc.ca>
2013-05-31 17:07:22 -0700

    added deliverable package, incremental package
    Former-commit-id: 14f5a613d8563eb2f4684a270b5fb476cad2fdf3 [formerly a8fdc51628c247589552750b8f8f496f6cf15e25]
    
    Former-commit-id: f41670862f9dec8a4169237954256f8e060faee1

--- FCCStationPacking/src/ca/ubc/cs/beta/stationpacking/execution/deliverable/Main.java
+++ FCCStationPacking/src/ca/ubc/cs/beta/stationpacking/execution/deliverable/Main.java
@@ -1,166 +1,151 @@
 package ca.ubc.cs.beta.stationpacking.execution.deliverable;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.util.HashSet;
-import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ca.ubc.cs.beta.stationpacking.data.*;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
+
+import ca.ubc.cs.beta.aclib.execconfig.AlgorithmExecutionConfig;
+import ca.ubc.cs.beta.aclib.misc.jcommander.JCommanderHelper;
+import ca.ubc.cs.beta.aclib.misc.options.UsageSection;
+import ca.ubc.cs.beta.aclib.options.AbstractOptions;
+import ca.ubc.cs.beta.aclib.options.ConfigToLaTeX;
+import ca.ubc.cs.beta.aclib.targetalgorithmevaluator.TargetAlgorithmEvaluator;
+import ca.ubc.cs.beta.aclib.targetalgorithmevaluator.init.TargetAlgorithmEvaluatorBuilder;
+import ca.ubc.cs.beta.aclib.targetalgorithmevaluator.init.TargetAlgorithmEvaluatorLoader;
+import ca.ubc.cs.beta.stationpacking.data.Station;
 import ca.ubc.cs.beta.stationpacking.data.manager.DACConstraintManager2;
 import ca.ubc.cs.beta.stationpacking.data.manager.DACStationManager;
-import ca.ubc.cs.beta.stationpacking.data.manager.IConstraintManager;
-import ca.ubc.cs.beta.stationpacking.data.manager.IStationManager;
-
-import ca.ubc.cs.beta.stationpacking.experiment.InstanceGeneration;
-import ca.ubc.cs.beta.stationpacking.experiment.experimentreport.IExperimentReporter;
-import ca.ubc.cs.beta.stationpacking.experiment.experimentreport.LocalExperimentReporter;
-
+import ca.ubc.cs.beta.stationpacking.execution.deliverable.parameters.MainParameterParser;
 import ca.ubc.cs.beta.stationpacking.experiment.instance.IInstance;
 import ca.ubc.cs.beta.stationpacking.experiment.instance.Instance;
 import ca.ubc.cs.beta.stationpacking.experiment.instanceencoder.cnfencoder.ICNFEncoder;
 import ca.ubc.cs.beta.stationpacking.experiment.instanceencoder.cnfencoder.CNFEncoder;
-
 import ca.ubc.cs.beta.stationpacking.experiment.instanceencoder.cnfresultlookup.HybridCNFResultLookup;
 import ca.ubc.cs.beta.stationpacking.experiment.instanceencoder.cnfresultlookup.ICNFResultLookup;
-
 import ca.ubc.cs.beta.stationpacking.experiment.solver.ISolver;
 import ca.ubc.cs.beta.stationpacking.experiment.solver.TAESolver;
-
-import ca.ubc.cs.beta.stationpacking.experiment.*;
+import ca.ubc.cs.beta.stationpacking.experiment.solver.result.SolverResult;
 
 public class Main {
-
-	//NA - TODO set random seed to get reproducible results.
-	private static Logger log = LoggerFactory.getLogger(Main.class);
-	private static final long fSeed = 13232;
 	
+	private static Logger log = LoggerFactory.getLogger(Main.class);
+
 	public static void main(String[] args) throws Exception {
-		//String dacDomainsFile = "/Users/narnosti/Documents/fcc-station-packing/Output/toy_domains.txt";
-		//String dacConstraintFile = "/Users/narnosti/Documents/fcc-station-packing/Output/toy_constraints.txt";
-		//String pairwiseConstraintsFile = "/Users/narnosti/Documents/fcc-station-packing/Output/Alex_pairwise_constraints.txt";
-
-		/*
-		String newDomainsFile = "/Users/narnosti/Documents/fcc-station-packing/Output/DAC_domains.txt";
-		String newConstraintsFile = "/Users/narnosti/Documents/fcc-station-packing/Output/DAC_constraints.txt";
-        */
-		/*
-		String StationsFile = "/Users/narnosti/Documents/FCCOutput/stations.csv";
-		String DomainsFile = "/Users/narnosti/Dropbox/Alex/2013 04 New Data/Domain 041813.csv";
-		String ConstraintsFile = "/Users/narnosti/Dropbox/Alex/2013 04 New Data/Interferences 041813.csv";
-		*/
 		
-		String StationsFile = "/Users/narnosti/Documents/FCCOutput/toy_stations.txt";
-		String DomainsFile = "/Users/narnosti/Documents/FCCOutput/toy_domains.txt";
-		String ConstraintsFile = "/Users/narnosti/Documents/FCCOutput/toy_constraints.txt";
+		/**
+		 * Test arguments to use, instead of compiling and using command line.
+		**/
 		
-		IStationManager aSM = new DACStationManager(StationsFile,DomainsFile);
-		IConstraintManager aCM = new DACConstraintManager2(aSM.getStations(),ConstraintsFile);
+		String[] aPaxosTargetArgs = {"-STATIONS_FILE",
+				"/ubc/cs/home/a/afrechet/arrow-space/workspace/FCCStationPackingExperimentDir/Data/stations2.csv",
+				"-DOMAINS_FILE",
+				"/ubc/cs/home/a/afrechet/arrow-space/workspace/FCCStationPackingExperimentDir/Data/NewDACData/Domain-041813.csv",
+				"-CONSTRAINTS_FILE",
+				"/ubc/cs/home/a/afrechet/arrow-space/workspace/FCCStationPackingExperimentDir/Data/NewDACData/Interferences-041813.csv",
+				"-CNF_DIR",
+				"/ubc/cs/home/a/afrechet/arrow-space/workspace/FCCStationPackingExperimentDir/CNFs",
+				"-SOLVER",
+				"tunedclasp",
+				"--execDir",
+				"SATsolvers",
+				/*
+				"--paramFile",
+				"SATsolvers/sw_parameterspaces/sw_tunedclasp.txt",
+				*/
+				"--algoExec",
+				"python solverwrapper.py",
+				"--cutoffTime",
+				"1800"
+				};
 		
-
-		//Set<Station> aStationSet = new HashSet<Station>();
+		args = aPaxosTargetArgs;
 		
-		Set<Integer> aChannels = new HashSet<Integer>();
-		for(int i = 14; i <=16; i++){
-			aChannels.add(i);
+	
+		/**
+		 * NA - do we want to remove the TAE options from the arguments specified on the command line?
+		**/
+		//TAE Options
+		Map<String,AbstractOptions> aAvailableTAEOptions = TargetAlgorithmEvaluatorLoader.getAvailableTargetAlgorithmEvaluators();
+		
+		//Parse the command line arguments in a parameter object.
+		MainParameterParser aExecParameters = new MainParameterParser();
+		JCommander aParameterParser = JCommanderHelper.getJCommander(aExecParameters, aAvailableTAEOptions);
+		try
+		{
+			aParameterParser.parse(args);
+		}
+		catch (ParameterException aParameterException)
+		{
+			List<UsageSection> sections = ConfigToLaTeX.getParameters(aExecParameters, aAvailableTAEOptions);
+			
+			boolean showHiddenParameters = false;
+			
+			//A much nicer usage screen than JCommander's 
+			ConfigToLaTeX.usage(sections, showHiddenParameters);
+			
+			log.error(aParameterException.getMessage());
+			return;
 		}
 		
+		//Use the parameters to instantiate the experiment.
+		log.info("Getting station information...");
+		DACStationManager aStationManager = new DACStationManager(aExecParameters.getRepackingDataParameters().getStationFilename(),aExecParameters.getRepackingDataParameters().getDomainFilename());
+	    
+		log.info("Getting constraint information...");
+		Set<Station> aStations = aStationManager.getStations();
+		DACConstraintManager2 dCM = new DACConstraintManager2(aStations,aExecParameters.getRepackingDataParameters().getConstraintFilename());
+	
+		log.info("Creating solver components...");
 
-
-		/*
-		log.info("Getting data...");
-		String aStationFileName = "/Users/narnosti/Documents/fcc-station-packing/Output/stations.csv";
-		String dacDomainsFile = "/Users/narnosti/Documents/fcc-station-packing/Input/Domains.csv";
-		DACStationManager aStationManager = new DACStationManager(aStationFileName,dacDomainsFile);
-        Set<Station> aStations = aStationManager.getStations();
-		String dacConstraintFile = "/Users/narnosti/Documents/fcc-station-packing/Input/Interferences-read-note-please.csv";
-		DACConstraintManager2 dCM = new DACConstraintManager2(aStations,dacConstraintFile);
-		*/
-		
 		ICNFEncoder aCNFEncoder = new CNFEncoder();
-		//System.out.println(aCNFEncoder.decode(new NInstance(aStationSet,aChannels),FileUtils.readFileToString(new File("/Users/narnosti/Documents/fcc-station-packing/CNFs/7ff9afd8a241da50dd85dc361ab701183c18e66445.out"))));
-		
-		
-		log.info("Creating cnf lookup...");
- 
-		String aCNFDir = "/Users/narnosti/Documents/fcc-station-packing/CNFs";
-		ICNFResultLookup aCNFLookup = new HybridCNFResultLookup(aCNFDir,"CNFOutput");
+		ICNFResultLookup aCNFLookup = new HybridCNFResultLookup(aExecParameters.getCNFDirectory(), aExecParameters.getCNFOutputName());
 				
 		log.info("Creating solver...");
-        String aParamConfigurationSpaceLocation = "/Users/narnosti/Documents/fcc-station-packing/FCCStationPacking/SATsolvers/sw_parameterspaces/sw_picosat.txt";
-		String aAlgorithmExecutable = "python solverwrapper.py";
-        String aExecDir = "/Users/narnosti/Documents/fcc-station-packing/FCCStationPacking/SATsolvers/";
-		int aMaximumConcurrentExecution = 4;
-		ISolver aSolver = new TAESolver(aCM, aCNFLookup, aCNFEncoder, aParamConfigurationSpaceLocation, aAlgorithmExecutable, aExecDir, "CLI",aMaximumConcurrentExecution, fSeed);
-		
-		log.info("Creating experiment reporter...");
-        String testFolder = "/Users/narnosti/Documents/fcc-station-packing/FCCStationPacking/ExperimentDir";
-		IExperimentReporter aExperimentReporter = new LocalExperimentReporter(testFolder, "test");
-		
-		Iterator<Station> aStationIterator = new InversePopulationStationIterator(aSM.getStations(), fSeed);
-		
-		log.info("Creating instance generation and beginning experiment...");
-		Set<Station> aStartingStations = new HashSet<Station>();
-		double aCutoff = 1800;
-		InstanceGeneration aInstanceGeneration = new InstanceGeneration(aSolver, aExperimentReporter);
-		aInstanceGeneration.run(aStartingStations, aStationIterator,aChannels,aCutoff);	
-		aCNFLookup.writeToFile();
-		File folder = new File(aCNFDir);
-		File[] aCNFFiles = folder.listFiles();
-		for(int i = 0; i < aCNFFiles.length; i++){
-			String aFileName = aCNFFiles[i].getName();
-			if(aFileName.endsWith("cnfoutput")){
-				System.out.println(aFileName);
-				IInstance aInstance = readInstanceFromCNFFile(aCNFDir+"/"+aFileName.substring(0,aFileName.indexOf("output")),new HashSet<Station>(aSM.getStations()));
-				Map<Integer,Set<Station>> aStationAssignment = aCNFEncoder.decode(aInstance, FileUtils.readFileToString(aCNFFiles[i]));
-				System.out.println(aStationAssignment);
-				if(!aCM.isSatisfyingAssignment(aStationAssignment)){
-					throw new Exception(aStationAssignment+" is not a valid assignment for instance "+aInstance);
-				}
-			}
+		//Logs the available target algorithm evaluators
+		for(String name : aAvailableTAEOptions.keySet())
+		{
+			log.info("Target Algorithm Evaluator Available: {} ", name);
 		}
 		
-	}
-
-
-private static IInstance readInstanceFromCNFFile(String aFileName, Set<Station> aStationSet) throws Exception{
-	BufferedReader aReader = new BufferedReader(new FileReader(aFileName));
-	Set<Station> aStations = new HashSet<Station>();
-	Set<Integer> aChannels = new HashSet<Integer>();
-	while(aReader.ready()){
-		String aLine = aReader.readLine();
-		if(aLine.startsWith("c Stations: ")){
-			String[] aSplitString = aLine.substring(aLine.indexOf(":")+1).split(" ");
-			for(int i = 0; i < aSplitString.length; i++){
-				//System.out.print(aSplitString[i]+" ");
-				if(aSplitString[i].length() > 0){
-					Integer aID = Integer.parseInt(aSplitString[i]);
-					aStations.add(new Station(aID, aChannels,0));
-				}
+		//Fix config space file based on solver
+		aExecParameters.getAlgorithmExecutionOptions().paramFileDelegate.paramFile = aExecParameters.getAlgorithmExecutionOptions().algoExecDir+File.separatorChar+"sw_parameterspaces"+File.separatorChar+"sw_"+aExecParameters.getSolver()+".txt";
+		AlgorithmExecutionConfig aTAEExecConfig = aExecParameters.getAlgorithmExecutionOptions().getAlgorithmExecutionConfig();
+		TargetAlgorithmEvaluator aTAE = null;
+		try {
+			aTAE = TargetAlgorithmEvaluatorBuilder.getTargetAlgorithmEvaluator(aExecParameters.getAlgorithmExecutionOptions().taeOpts, aTAEExecConfig, false, aAvailableTAEOptions);
+			ISolver aSolver = new TAESolver(dCM, aCNFLookup, aCNFEncoder, aTAE, aTAEExecConfig,aExecParameters.getSeed());			
+			while(true){ //There must be a way to do this that puts fewer demands on the processor
+				//if(a message has arrived)
+					//if(the message encodes an instance) 
+						IInstance aInstance = decodeInstance(/*message*/);
+						SolverResult aSolverResult = aSolver.solve(aInstance,1800/*we could pass CutoffTime to the solver in its constructor*/);
+						//Send aSolverResult back
+					//else if(the message tells us to quit)
+						//quit
+					//else
+						//send "message not understood" reply
 			}
-			aStationSet.retainAll(aStations);
-			if(aStationSet.size()!=aStations.size()){
-				aReader.close();
-				throw new Exception("When decoding: some stations not found in stations file");
-			}
-		} else if(aLine.startsWith("c Channels: ")){
-			String[] aSplitString = aLine.substring(aLine.indexOf(":")+1).split(" ");
-			for(int i = 0; i < aSplitString.length; i++){
-				if(aSplitString[i].length() > 0){
-					aChannels.add(Integer.parseInt(aSplitString[i]));
-				}
-			}
+		} 
+		finally
+		{
+			//We need to tell the TAE we are shutting down, otherwise the program may not exit 
+			if(aTAE != null){ aTAE.notifyShutdown();}
 		}
+
 	}
-	aReader.close();
-	IInstance aInstance = new Instance(aStationSet,aChannels);
-	return aInstance;
+	
+	//NA - Alex: fill in here!
+	private static IInstance decodeInstance(/*message*/){
+		return new Instance(new HashSet<Station>(),new HashSet<Integer>());
+	}
 }
-}
+
 
