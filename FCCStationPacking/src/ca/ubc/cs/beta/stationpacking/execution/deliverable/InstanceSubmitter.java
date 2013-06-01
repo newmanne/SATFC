fa8fe7b22144c1af694a9440014ba69b69d7dc89
Nick Arnosti <narnosti@host47-214.wifi.ubc.ca>
2013-05-31 17:07:22 -0700

    added deliverable package, incremental package
    Former-commit-id: a8fdc51628c247589552750b8f8f496f6cf15e25

---
+++ FCCStationPacking/src/ca/ubc/cs/beta/stationpacking/execution/deliverable/InstanceSubmitter.java
@@ -0,0 +1,33 @@
+package ca.ubc.cs.beta.stationpacking.execution.deliverable;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class InstanceSubmitter {
+
+	public InstanceSubmitter(String[] instanceEncoding){
+		if(instanceEncoding.length == 2){
+			String[] aStationStrings = instanceEncoding[0].substring(instanceEncoding[0].indexOf(":")+1).split(",");
+			Set<Integer> aStationIDs = stringArrayToIntegerSet(aStationStrings);
+			String[] aChannelStrings = instanceEncoding[1].substring(instanceEncoding[1].indexOf(":")+1).split(",");
+			Set<Integer> aChannels = stringArrayToIntegerSet(aChannelStrings);
+			//Send message with aStationIDs, aChannels
+			//Receive assignment back and output it in some format
+		} else {
+			//Throw some error - bad parameters provided
+		}
+	}
+	
+	private static Set<Integer> stringArrayToIntegerSet(String[] aStrings){
+		Set<Integer> aIntegers = new HashSet<Integer>();
+		for(int i = 0; i < aStrings.length; i++){
+			if(aStrings[i].length() > 0){
+				Integer aID = Integer.parseInt(aStrings[i]);
+				aIntegers.add(aID);
+			}
+		}
+		//Catch bad format if parseInt fails
+		return aIntegers;
+	}
+}
