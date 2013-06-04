c1597d3dd90edd65d5f0f391bf2755401d024f09
Nick Arnosti <narnosti@rescomp-11-219594.stanford.edu>
2013-06-04 13:13:16 -0700

    final synch
    Former-commit-id: 5b197da1468d2e230b187972823e5c88b3ad2a95 [formerly bf476e622c3f4cbd057f86749e3c032adc0eb3af]
    
    Former-commit-id: 1043a9da0db385436da2bb7b34b60262a263aadf

---
+++ FCCStationPacking/SATsolvers/glueminisat-2.2.5/simp/depend.mk
@@ -0,0 +1,65 @@
+/Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/Main.o /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/Main.or /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/Main.od /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/Main.op: \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/Main.cc \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/utils/System.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/IntTypes.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/utils/ParseUtils.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/utils/Options.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Vec.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/XAlloc.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/core/Dimacs.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/core/SolverTypes.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Alg.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Map.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Alloc.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/SimpSolver.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Queue.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/core/Solver.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Heap.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/core/RecentVals.h
+/Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/SimpSolver.o /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/SimpSolver.or /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/SimpSolver.od /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/SimpSolver.op: \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/SimpSolver.cc \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Sort.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Vec.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/IntTypes.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/XAlloc.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/SimpSolver.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Queue.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/core/Solver.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Heap.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Alg.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/utils/Options.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/utils/ParseUtils.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/core/SolverTypes.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Map.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Alloc.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/core/RecentVals.h \
+  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/utils/System.h
+/Users/narnosti/Downloads/glueminisat-2.2.5/core/Main.o /Users/narnosti/Downloads/glueminisat-2.2.5/core/Main.or /Users/narnosti/Downloads/glueminisat-2.2.5/core/Main.od /Users/narnosti/Downloads/glueminisat-2.2.5/core/Main.op: /Users/narnosti/Downloads/glueminisat-2.2.5/core/Main.cc \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/utils/System.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/IntTypes.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/utils/ParseUtils.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/utils/Options.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Vec.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/XAlloc.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/core/Dimacs.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/core/SolverTypes.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Alg.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Map.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Alloc.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Heap.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/core/RecentVals.h
+/Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.o /Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.or /Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.od /Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.op: /Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.cc \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Sort.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Vec.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/IntTypes.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/XAlloc.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Heap.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Alg.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/utils/Options.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/utils/ParseUtils.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/core/SolverTypes.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Map.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Alloc.h \
+  /Users/narnosti/Downloads/glueminisat-2.2.5/core/RecentVals.h
