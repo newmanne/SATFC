7ab8b4227aad0b2796df59a77bdaa29773d1027c
Nick Arnosti <narnosti@rescomp-11-219594.stanford.edu>
2013-06-04 12:53:29 -0700

    adding newly archived code
    Former-commit-id: dfe9c7e0b8a2b03fca1eaaf90c3b2da2182a3b3e [formerly 361c0ca3f52310163d71005f92f12a0571a326a8]
    
    Former-commit-id: 341de79dcd42a5099c1f24ca117b8cb88dce4301

---
+++ FCCStationPacking/archive/IInstance.java
@@ -0,0 +1,44 @@
+package ca.ubc.cs.beta.stationpacking.datastructures;
+
+import java.util.Set;
+
+
+/**
+ * A station packing problem instance container object.
+ * @author afrechet, narnosti
+ *
+ */
+public interface IInstance {
+	
+	/* Returns the set of stations associated with the instance.
+	 * Never null, though it may be empty.
+	 */
+	public Set<Station> getStations();
+	
+	/* Adds aStation to the set of stations.
+	 * Returns true if the set did not previously contain aStation.
+	 */
+	public boolean addStation(Station aStation);
+	
+	/* Adds aStation to the set of stations.
+	 * Returns true if the set previously contained aStation.
+	 */
+	public boolean removeStation(Station aStation);
+	
+	/* Returns the set of channels associated with the instance.
+	 */
+	public Set<Integer> getChannelRange();
+	
+	/*
+	 * 
+	 */
+	
+	public Station getStation(Integer aID);
+	
+	@Override
+	public String toString();
+
+	public int getNumberofStations();
+	
+	
+}
