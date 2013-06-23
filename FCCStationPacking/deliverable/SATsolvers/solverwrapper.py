44d2aa431e87cf1bb5b87c64371af4cde3251a98
afrechet <afrechet@paxos.cs.ubc.ca>
2013-06-23 16:57:59 -0700

    Optimized solver wrapper code (temp_CNF writing process was very slow).
    Added info messages (independent verifier, ...)
    
    Former-commit-id: aa2e82e0efb628f235461cd3bdb9c3dd867dd344 [formerly 18d6c5699f50cee6f40bf849715ad1563eede076]
    
    Former-commit-id: 2f9e330f5bbd14bb0cad46d59965435f4ad14788

--- FCCStationPacking/deliverable/SATsolvers/solverwrapper.py
+++ FCCStationPacking/deliverable/SATsolvers/solverwrapper.py
@@ -102,13 +102,9 @@ elif solvername == 'picosat':
 else:
     print 'ERROR, invalid solver name ',solvername
 
-
-(instance_name_head,instance_name_tail) = os.path.split(instance_name)
-
 #####################################################################################################
 #Make a new CNF where variables are named from 1 to n (where n is the total number of variables.
 #Highly undesirable fix - should use a better variable identification scheme inside the (java) solver.
-
 instance_file = open(instance_name)
 instance_lines = instance_file.readlines()
 
@@ -116,13 +112,6 @@ variables = []
 variables.append(0)
 
 formatted_lines = []
-
-print len(instance_lines)
-
-variable_map = dict()
-variable_index = 1
-
-t = time.time()
 for line in instance_lines:
     line = line.replace('\n','')
     if not (line[0]=='p' or line[0]=='c'):
@@ -133,21 +122,18 @@ for line in instance_lines:
         for litteral in litterals:
             negated = litteral < 0
             litteral = abs(litteral)
-            if litteral not in variable_map:
-                variable_map[litteral] = variable_index
-                variable_index = variable_index + 1 
-            mapped_litterals.append((1-2*negated)*variable_map[litteral])
+            if litteral not in variables:
+                variables.append(litteral)
+            mapped_litterals.append((1-2*negated)*variables.index(litteral))
         formatted_line = ' '.join(map(lambda l : str(l),mapped_litterals))+' 0\n'
         formatted_lines.append(formatted_line)
+formatted_lines.insert(0,'p cnf '+str(len(variables)-1)+' '+str(len(formatted_lines))+'\n')
 
-
-formatted_lines.insert(0,'p cnf '+str(len(variable_map))+' '+str(len(formatted_lines))+'\n')
-
+(instance_name_head,instance_name_tail) = os.path.split(instance_name)
 temp_CNF = tempfile.NamedTemporaryFile(dir=instance_name_head,delete=True)
 temp_CNF.write(''.join(formatted_lines))
 temp_CNF.flush()
 instance_name = temp_CNF.name
-print str(time.time()-t)
 ####################################################################################################
 
 temp_result = tempfile.NamedTemporaryFile(dir=instance_name_head, delete=True)
@@ -211,9 +197,8 @@ elif re.search(SATre,std_out):
         assignment.remove('0')
 
 ####################################################################################################
-    #Remap temp variables from assignment to original variables
-    inv_variable_map = {v:k for k,v in variable_map.iteritems()}
-    assignment = map(lambda l : str((1-2*(int(l)<0))*inv_variable_map[abs(int(l))]),assignment)
+    #Remap variables from temp CNF to variables provided by 
+    assignment = map(lambda l : str((1-2*(int(l)<0))*variables[abs(int(l))]),assignment)
 ####################################################################################################
 
     assignment = ';'.join(assignment)
@@ -229,6 +214,3 @@ if output_solved == 'SAT':
 else:
     print 'Result for ParamILS: '+output_solved+', '+output_runtime+', '+output_runlength+', '+output_quality+', '+output_seed+'\n'
 
-temp_CNF.close()
-temp_result.close()
-sys.exit()
