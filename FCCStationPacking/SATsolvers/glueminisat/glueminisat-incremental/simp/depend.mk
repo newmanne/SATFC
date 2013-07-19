89888f44ff1e841d258cef8401874e2bf0c888db
Arpit Goel <argoel@stanford.edu>
2013-07-18 21:06:50 -0700

    IncrementalSolverExecutor Code
    Former-commit-id: 10c5638b0d330e5027ed44897b4a5ea5468cf470 [formerly 3f5a7a81669cf477965b4fd50d3d40c3864ba751]
    
    Former-commit-id: 59e5234ca684ead4d5733c11a4dd9848770d768a

--- FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/simp/depend.mk
+++ FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/simp/depend.mk
@@ -1,65 +1,36 @@
-/Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/Main.o /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/Main.or /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/Main.od /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/Main.op: \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/Main.cc \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/utils/System.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/IntTypes.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/utils/ParseUtils.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/utils/Options.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Vec.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/XAlloc.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/core/Dimacs.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/core/SolverTypes.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Alg.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Map.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Alloc.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/SimpSolver.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Queue.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/core/Solver.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Heap.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/core/RecentVals.h
-/Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/SimpSolver.o /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/SimpSolver.or /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/SimpSolver.od /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/SimpSolver.op: \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/SimpSolver.cc \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Sort.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Vec.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/IntTypes.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/XAlloc.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/simp/SimpSolver.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Queue.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/core/Solver.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Heap.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Alg.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/utils/Options.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/utils/ParseUtils.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/core/SolverTypes.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Map.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/mtl/Alloc.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/core/RecentVals.h \
-  /Users/narnosti/Documents/fcc-station-packing/glueminisat-2.2.5/utils/System.h
-/Users/narnosti/Downloads/glueminisat-2.2.5/core/Main.o /Users/narnosti/Downloads/glueminisat-2.2.5/core/Main.or /Users/narnosti/Downloads/glueminisat-2.2.5/core/Main.od /Users/narnosti/Downloads/glueminisat-2.2.5/core/Main.op: /Users/narnosti/Downloads/glueminisat-2.2.5/core/Main.cc \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/utils/System.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/IntTypes.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/utils/ParseUtils.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/utils/Options.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Vec.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/XAlloc.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/core/Dimacs.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/core/SolverTypes.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Alg.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Map.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Alloc.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Heap.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/core/RecentVals.h
-/Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.o /Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.or /Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.od /Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.op: /Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.cc \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Sort.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Vec.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/IntTypes.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/XAlloc.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/core/Solver.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Heap.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Alg.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/utils/Options.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/utils/ParseUtils.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/core/SolverTypes.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Map.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/mtl/Alloc.h \
-  /Users/narnosti/Downloads/glueminisat-2.2.5/core/RecentVals.h
+/Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/simp/Main.o /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/simp/Main.or /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/simp/Main.od /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/simp/Main.op: \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/simp/Main.cc \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/utils/System.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/mtl/IntTypes.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/utils/ParseUtils.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/utils/Options.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/mtl/Vec.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/mtl/XAlloc.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/core/Dimacs.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/core/SolverTypes.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/mtl/Alg.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/mtl/Map.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/mtl/Alloc.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/simp/SimpSolver.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/mtl/Queue.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/core/Solver.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/mtl/Heap.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/core/RecentVals.h
+/Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/simp/SimpSolver.o /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/simp/SimpSolver.or /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/simp/SimpSolver.od /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/simp/SimpSolver.op: \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/simp/SimpSolver.cc \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/mtl/Sort.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/mtl/Vec.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/mtl/IntTypes.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/mtl/XAlloc.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/simp/SimpSolver.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/mtl/Queue.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/core/Solver.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/mtl/Heap.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/mtl/Alg.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/utils/Options.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/utils/ParseUtils.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/core/SolverTypes.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/mtl/Map.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/mtl/Alloc.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/core/RecentVals.h \
+  /Users/arpit/Documents/webDev/FCC/fcc-station-packing/FCCStationPacking/SATsolvers/glueminisat/glueminisat-incremental/utils/System.h
--- FCCStationPacking/src/ca/ubc/cs/beta/stationpacking/execution/parameters/solver/IncrementalSolverParameters.java
+++ FCCStationPacking/src/ca/ubc/cs/beta/stationpacking/execution/parameters/solver/IncrementalSolverParameters.java
@@ -1,11 +1,17 @@
 package ca.ubc.cs.beta.stationpacking.execution.parameters.solver;
 
+import ca.ubc.cs.beta.aclib.options.AbstractOptions;
 import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParametersDelegate;
 
-public class IncrementalSolverParameters {
-	
-	
-	@Parameter(names = "-LIBRARY", description = "Path to incremental SAT library.")
+public class IncrementalSolverParameters extends AbstractOptions {
+
+
+    //ExecutableSolver parameters
+    @ParametersDelegate
+    public ExecutableSolverParameters ExecutableSolverParameters = new ExecutableSolverParameters();
+
+    @Parameter(names = "-LIBRARY", description = "Path to incremental SAT library.")
 	private String fLibraryPath;
 	public String getIncrementalLibraryLocation(){
 		//Insert check here? I want it to be required IF we're using an incremental solver.
