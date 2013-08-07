05f476b2810789094d1873d46eb6533c3a91ac18
Guillaume Saulnier-Comte <saulnier.guillaume@gmail.com>
2013-08-07 15:33:32 -0400

    Test class to load libraries with jna.
    Former-commit-id: cec3860cff93a80690ff3dee050bb02bd562bbed [formerly 7caa82594210ed4146dc83ca4f11e76992ab3c32]
    
    Former-commit-id: 615bc9e07ca1763ed81d36469fb6ee66b9a6736a

---
+++ src/ca/ubc/cs/beta/stationpacking/Test_Native_Load.java
@@ -0,0 +1,19 @@
+package ca.ubc.cs.beta.stationpacking;
+
+import ca.ubc.cs.beta.stationpacking.solver.jnalibraries.GlueMiniSATLibrary;
+
+import com.sun.jna.Native;
+
+public class Test_Native_Load {
+
+	/**
+	 * Run this main class to check if you are able to load libraries using jna.
+	 * @param args args[0] path to the library you are trying to load with jna.
+	 */
+	public static void main(String[] args)
+	{
+		String lib = args[0];
+		Native.loadLibrary(lib, GlueMiniSATLibrary.class);
+	}
+	
+}
