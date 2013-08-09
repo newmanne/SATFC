dfaef9f2890703bf31a825b5ac5a725f436331f7
Guillaume Saulnier-Comte <saulnier.guillaume@gmail.com>
2013-08-09 19:38:55 -0400

    Update to clasp JNA.
    Former-commit-id: a1601ea853771f5e2da6822952625ac99dd3b2d2 [formerly 78dfa6a8c285cfc63c50589f61398d56d56f2a6d]
    
    Former-commit-id: 7c27938fcaa7dd2505c49b80281d609a33361a36

--- SATsolvers/clasp/jna/jna_clasp.cpp
+++ SATsolvers/clasp/jna/jna_clasp.cpp
@@ -1,11 +1,103 @@
 // Guillaume Saulnier-Comte
+#include <iostream>
+#include <sstream>
 
 #include "jna_clasp.h"
+#include "clasp/reader.h"
 
 namespace JNA {
 
-// JNAConfig specific functions
-JNAConfig::JNAConfig() {}
+// JNAConfig
+
+JNAConfig::JNAConfig() : status_(c_not_configured) {}
+
+JNAConfig::Conf_Status JNAConfig::getStatus()
+{
+	return status_;
+}
+
+std::string JNAConfig::getErrorMessage()
+{
+	return err_message_;
+}
+
+std::string JNAConfig::getClaspErrorMessage()
+{
+	return messages.error;
+}
+
+// call to configure the JNAConfig
+void JNAConfig::configure(char* args, int maxArgs)
+{
+	if (status_ != c_not_configured)
+	{
+		config_.reset();
+	}
+	err_message_ = "No error.";
+	// first we need to simulate the argc and argv that are given to the run function in clasp_app.h run().
+	int argc = 1;
+	char* argv[maxArgs];
+	
+	char* arg = strtok(args, " ");
+	while (arg && argc < maxArgs)
+	{
+		argv[argc] = arg;
+		argc++;
+		arg = strtok(0, " ");
+	}
+
+	// too many arguments present
+	if (argc >= maxArgs)
+	{
+		status_ = c_error;
+		err_message_ = "Too many arguments were given, call the constructor with a higer value of maxArgs! (defaul=128)";
+	}
+
+	// call the init options to set up the configuration
+	bool value = parse(argc, argv, "jna_clasp", Clasp::parsePositional);
+
+	// set the error function
+	if (value)
+	{
+		status_ = c_valid;
+	}
+	else
+	{
+		status_ = c_error;
+		err_message_ = "Parsing of the command line arguments failed!  Please test with the clasp executable.";
+	}
+}
+
+ClaspConfig* JNAConfig::getConfig()
+{
+	return &config_;
+}
+
+// JNAProblem
+
+JNAProblem::JNAProblem(std::string problem)
+{
+	problem_ = problem;
+}
+
+bool JNAProblem::read(ApiPtr p, uint32 properties) {
+	std::istringstream istr(problem_);
+	status_ = Clasp::parseDimacs(istr, *(p.ctx)); 
+	return status_;
+}
+
+bool JNAProblem::getStatus()
+{
+	return status_;
+}
+
+
+
+
+
+
+
+
 
 
 }
--- SATsolvers/clasp/jna/jna_clasp.h
+++ SATsolvers/clasp/jna/jna_clasp.h
@@ -3,7 +3,10 @@
 #ifndef JNA_CLASP_H
 #define JNA_CLASP_H
 
+#include <string>
+
 #include "clasp_options.h"
+#include "clasp/clasp_facade.h"
 
 using namespace Clasp;
 namespace JNA {
@@ -11,8 +14,18 @@ namespace JNA {
 class JNAConfig : public ProgramOptions::AppOptions {
 public:
 	JNAConfig();
+	// status of the ClaspConfig object
 	enum Conf_Status { c_not_configured, c_valid, c_error };
+
+	Conf_Status getStatus(); //return the status of the JNA Config
+	std::string getErrorMessage();
+	std::string getClaspErrorMessage();
+	void configure(char* args, int maxArgs=128); // use the args to configure config_
+
+	ClaspConfig* getConfig();
+
 private:
+
 	// -------------------------------------------------------------------------------------------
 	// AppOptions interface
 	void	printHelp(const ProgramOptions::OptionContext& root)	{};
@@ -26,14 +39,53 @@ private:
         }
         // -------------------------------------------------------------------------------------------
 
-	ClaspConfig		config_;
-	ClaspOptions		clasp_;
-	Conf_Status		status_;
+	ClaspConfig	config_;
+	ClaspOptions	clasp_;
+	Conf_Status	status_;
+	std::string	err_message_;
+};
+
+
+// JNAProblem holder
+class JNAProblem : public Clasp::Input {
+public:
+	JNAProblem(std::string problem);
+	Format format() const { return DIMACS; }
+	bool	read(ApiPtr api, uint32 properties);
+	void	addMinimize(Clasp::MinimizeBuilder&, ApiPtr) {}
+	void	getAssumptions(Clasp::LitVec&) {}
+	bool	getStatus(); // returns the status of the problem after solve has been called
+private:
+	std::string problem_;
+	bool status_;
+};
+
+// Callback to set up the required information (i.e. results)
+
+class JNAResults : public Clasp::ClaspFacade::Callback {
+public:
+	// called when a state is entered or left
+	void state(Event e, ClaspFacade& f) {};
+
+	// Some operation triggered an important event.
+	/*
+	* \param s The solver that triggered the event.
+	* \param e An event that is neither event_state_enter nor event_state_exit.
+	*/
+	void event(const Solver& s, Event e, ClaspFacade& f);
+
+	//! Some configuration option is unsafe/unreasonable w.r.t the current problem.
+	void warning(const char* msg);
+
+	bool getInterrupt();
+	bool setInterrupt();
+	bool unsetInterrupt();
+private:
+	bool interupt_;
 
 };
 
 
 
 }
-
 #endif
