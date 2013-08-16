ad515701f9d06689f16096a087e09ca736b5a17c
Guillaume Saulnier-Comte <saulnier.guillaume@gmail.com>
2013-08-16 15:47:38 -0400

    Added compile.sh script to compile libjnaclasp.so.  Cleaned up imports.
    Former-commit-id: a6099869ca00173d592de32a561e5b14ce456c95 [formerly 2c5a4c21e7180b5f5a678b4965e2eaf336027f5a]
    
    Former-commit-id: 2cfd8182757b56b2b59bf92fcd179d5f650bf3b6

--- src/ca/ubc/cs/beta/stationpacking/execution/NewSolverTest.java
+++ src/ca/ubc/cs/beta/stationpacking/execution/NewSolverTest.java
@@ -82,14 +82,14 @@ public class NewSolverTest {
 			
 			String aAlexLibPath = "/ubc/cs/project/arrow/afrechet/git/FCCStationPacking/SATsolvers/clasp/jna/libjnaclasp.so";
 			String aSantaLibPath = "/home/gsauln/workspace/FCC-Station-Packing/SATsolvers/clasp/jna/libjnaclasp.so";
-			ISATSolver aClaspSATSolver = new ClaspSATSolver(aAlexLibPath, 
+			ISATSolver aClaspSATSolver = new ClaspSATSolver(aSantaLibPath, 
 					 "--eq=0 --trans-ext=all --sat-prepro=0 --sign-def=2 --del-max=10000 --strengthen=local,1 --del-init-r=800,20000 --loops=no --init-watches=0 --heuristic=Berkmin --del-cfl=F,100 --restarts=L,256 --del-algo=basic,0 --deletion=3,66,3.0 --berk-max=256 --del-grow=1.0,100.0,F,128 --update-act --del-glue=4,0 --update-lbd=2 --reverse-arcs=3 --otfs=0 --berk-huang --del-on-restart=50 --contraction=120 --counter-restarts=3 --local-restarts --lookahead=no --save-progress=10 --counter-bump=180");
 			
 			/*
 			 * 
 			 */
 			
-			ISolver aSATBasedSolver = new SATBasedSolver(aTAESATSolver, new SATEncoder(aStationManager, aConstraintManager), aConstraintManager, new NoGrouper());
+			ISolver aSATBasedSolver = new SATBasedSolver(aClaspSATSolver, new SATEncoder(aStationManager, aConstraintManager), aConstraintManager, new NoGrouper());
 			
 		
 			aInstanceGeneration = new InstanceGeneration(aSATBasedSolver,aInstanceGenerationParameters.getExperimentReporter());
