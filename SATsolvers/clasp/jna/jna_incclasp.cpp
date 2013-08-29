1937f244ac789b5db8b372c8e84c410c61276681
Guillaume Saulnier-Comte <saulnier.guillaume@gmail.com>
2013-08-29 19:46:05 -0400

    Incremental Solving with clasp now working.  Seems to be bug free.
    Former-commit-id: 4f6bc943e7f3687ba98a9431b8302fe51ab7d479

--- SATsolvers/clasp/jna/jna_incclasp.cpp
+++ SATsolvers/clasp/jna/jna_incclasp.cpp
@@ -1,17 +1,109 @@
 // Guillaume Saulnier-Comte
 #include <iostream>
+#include <sstream>
+#include <climits>
 
 #include "jna_incclasp.h"
+#include <clasp/clause.h>
 
 namespace JNA
 {
+
+bool parseLitVec(StreamSource& in, LitVec& vec)
+{
+	Literal rLit;
+	int lit = -1;
+	while (in.parseInt(lit))
+	{
+		if (lit == 0)
+		{
+			return true;
+		}
+		else
+		{
+			rLit = lit >= 0 ? posLit(lit) : negLit(-lit);
+			vec.push_back(rLit);
+		}
+	}
+	return false;
+}
 
 JNAIncProblem::JNAIncProblem(const char* (*readCallback)()) : readCallback_(readCallback) {}
 
 bool JNAIncProblem::read(ApiPtr api, uint32 properties)
 {
-	std::cout << readCallback_() << std::endl;
-	return true;
+	const char* problem = readCallback_();
+
+	// initialize variables
+	std::istringstream istr(problem);
+	StreamSource in(istr);
+
+	SharedContext& ctx = *(api.ctx);
+	ClauseCreator cc(ctx.master());
+
+	int state = 1; // 1, we need to read the p line; 2, we need to read the a line; 3 we need to read the new clauses, 0 stop and parsing completed successfully, -1..-n stop and an error occured
+
+	int numClauses = 0;
+	int clauseCount = 0;
+	LitVec clause;
+
+	// start parsing the message and update the problem
+	in.skipWhite();
+	if (match(in, "exit", false))
+	{
+		return false;
+	}
+	// parse the problem
+	while (state > 0)
+	{
+		in.skipWhite();
+		if (*in == 'c') { skipLine(in); continue; } // skip comments (should not have any but...)
+		if (state == 1) // read the p line
+		{
+			if (!match(in, "p ", false)) { state = -1; continue;}
+			if (!match(in, "pcnf ", false)) { state = -2; continue;}
+			int totalVars = 0;
+			// check the total number of vars and increase the count in the ctx
+			if (!in.parseInt(totalVars, 1, INT_MAX)) { state = -3; continue;}
+			while (ctx.numVars() < totalVars)
+			{
+				ctx.addVar(Var_t::atom_var);
+			}
+			ctx.symTab().startInit();
+                        ctx.symTab().endInit(SymbolTable::map_direct, totalVars+1);
+
+			// set the number of clauses to read.
+			if (!in.parseInt(numClauses, 0, INT_MAX)) { state = -4; continue;}
+			state = 2;
+		}
+		else if (state == 2) // read the a line
+		{
+			assumptions_.clear();
+			if (!match(in, "a ", false)) { state = -5; continue;}
+			if (!parseLitVec(in, assumptions_)) { state = -6; continue;}
+			state = 3;
+			ctx.startAddConstraints();
+		}
+		else if (state == 3) // read the new clause
+		{
+			if (clauseCount == numClauses) { state = 0; continue;}
+			clause.clear();
+			if (!parseLitVec(in, clause)) { state = -7; continue;}
+
+			// add clause to context
+			cc.start();
+			for (LitVec::iterator it = clause.begin(); it != clause.end(); ++it) 
+			{
+				cc.add(*it);
+                        } 
+                        if (!cc.end()) { state = -8; continue;}
+
+			clauseCount++;
+		}
+	}
+
+	if (state == 0) return true;
+	return false;
 }
 
 void JNAIncProblem::getAssumptions(Clasp::LitVec& vec) 
@@ -19,15 +111,20 @@ void JNAIncProblem::getAssumptions(Clasp::LitVec& vec)
 	vec = assumptions_;
 }
 
-JNAIncControl::JNAIncControl(int (*nextCallback)()) : nextCallback_(nextCallback) {}
+JNAIncControl::JNAIncControl(int (*nextCallback)(), JNAResult* result) : nextCallback_(nextCallback), result_(result) {}
+
+bool JNAIncControl::nextStep(ClaspFacade& f)
+{
+	if (result_->getState() != JNAResult::r_SAT)
+	{
+		result_->setState(JNAResult::r_UNSAT);
+	}
+	return nextCallback_();
+}
 
 void solveIncremental(JNAFacade &facade, JNAIncProblem& problem, JNAConfig& config, JNAIncControl& inc, JNAResult &result)
 {
         facade.solveIncremental(problem, *(config.getConfig()), inc, &result);
-        if (result.getState() != JNAResult::r_SAT)
-        {
-                result.setState(JNAResult::r_UNSAT);
-        }
 }
 
 } //end JNA namespace
@@ -43,9 +140,10 @@ void destroyIncProblem(void* _problem)
 	delete reinterpret_cast<JNA::JNAIncProblem*>(_problem);
 }
 
-void* createIncControl(int (*_nextCallback)())
+void* createIncControl(int (*_nextCallback)(), void* _result)
 {
-	JNA::JNAIncControl* control = new JNA::JNAIncControl(_nextCallback);
+	JNA::JNAResult* result = reinterpret_cast<JNA::JNAResult*>(_result);
+	JNA::JNAIncControl* control = new JNA::JNAIncControl(_nextCallback, result);
 	return control;
 }
 
@@ -66,12 +164,3 @@ void jnasolveIncremental(void* _facade, void* _problem, void* _config, void* _co
         JNA::solveIncremental(*facade, *problem, *config, *control, *result);
 }
 
-
-void test(const char* (*fn)(), int (*fn1)())
-{
-	JNA::JNAIncProblem problem(fn);
-	Clasp::SharedContext* ctx = 0;
-	Clasp::Input::ApiPtr ptr(ctx);
-	problem.read(ptr,0);
-	std::cout << fn1() << std::endl;
-}
