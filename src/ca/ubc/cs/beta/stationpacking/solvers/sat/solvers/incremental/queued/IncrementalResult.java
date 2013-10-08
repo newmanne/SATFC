27a28bb4eb3e6525570cad9977d7c1d7a27865de
Guillaume Saulnier-Comte <saulnier.guillaume@gmail.com>
2013-10-08 15:31:02 -0400

    Incremental Clasp Consumer implemented (missing termination and testing).
    Former-commit-id: a17e6124103150c724f878e7f629961a124ab7b7

---
+++ src/ca/ubc/cs/beta/stationpacking/solvers/sat/solvers/incremental/queued/IncrementalResult.java
@@ -0,0 +1,30 @@
+package ca.ubc.cs.beta.stationpacking.solvers.sat.solvers.incremental.queued;
+
+import ca.ubc.cs.beta.stationpacking.solvers.base.SATResult;
+
+public class IncrementalResult {
+
+	private final SATResult fSATResult;
+	private final String fAssignment;
+	private final double fRuntime;
+	
+	public IncrementalResult(SATResult satResult, String assignment, double runtime)
+	{
+		fSATResult = satResult;
+		fAssignment = assignment;
+		fRuntime = runtime;
+	}
+	
+	public SATResult getSATResult() {
+		return fSATResult;
+	}
+	public String getAssignment() {
+		return fAssignment;
+	}
+	public double getRuntime() {
+		return fRuntime;
+	}
+	
+	
+	
+}
