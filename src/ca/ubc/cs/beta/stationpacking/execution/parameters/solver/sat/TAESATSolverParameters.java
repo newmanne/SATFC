b6b148ddf87f0baef0563a58a0688b5514d014a0
afrechet <afrechet@cs.ubc.ca>
2014-01-06 19:31:17 -0500

    Preparing to support channel specific data format.
    Former-commit-id: d583443a80d08df343feda62c015161cba931fcc [formerly 68338fe08ec5e9d69f96c69367903eea14f0bc4a]
    
    Former-commit-id: 6f82bee42de795495a0f96679f543350b80d5b11

--- src/ca/ubc/cs/beta/stationpacking/execution/parameters/solver/sat/TAESATSolverParameters.java
+++ src/ca/ubc/cs/beta/stationpacking/execution/parameters/solver/sat/TAESATSolverParameters.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import ca.ubc.cs.beta.aclib.execconfig.AlgorithmExecutionOptions;
 import ca.ubc.cs.beta.aclib.misc.options.UsageTextField;
 import ca.ubc.cs.beta.aclib.options.AbstractOptions;
+import ca.ubc.cs.beta.aclib.targetalgorithmevaluator.TargetAlgorithmEvaluator;
 import ca.ubc.cs.beta.aclib.targetalgorithmevaluator.init.TargetAlgorithmEvaluatorBuilder;
 import ca.ubc.cs.beta.aclib.targetalgorithmevaluator.init.TargetAlgorithmEvaluatorLoader;
 import ca.ubc.cs.beta.stationpacking.execution.parameters.validator.ImplementedSolverParameterValidator;
@@ -36,10 +37,13 @@ public class TAESATSolverParameters extends AbstractOptions implements ISATSolve
 		
 		AlgorithmExecutionOptions.paramFileDelegate.paramFile = AlgorithmExecutionOptions.algoExecDir+File.separatorChar+"sw_parameterspaces"+File.separatorChar+"sw_"+Solver+".txt";
 		
-		return new TAESATSolver(
-				TargetAlgorithmEvaluatorBuilder.getTargetAlgorithmEvaluator(AlgorithmExecutionOptions.taeOpts,
-						false,
-						AvailableTAEOptions),
+		@SuppressWarnings("deprecation")
+		TargetAlgorithmEvaluator aTAE = TargetAlgorithmEvaluatorBuilder.getTargetAlgorithmEvaluator(AlgorithmExecutionOptions.taeOpts,
+				false,
+				AvailableTAEOptions);
+		
+		return new TAESATSolver(aTAE
+				,
 				AlgorithmExecutionOptions.getAlgorithmExecutionConfig(null).getParamFile().getDefaultConfiguration(),
 				AlgorithmExecutionOptions.getAlgorithmExecutionConfig(null),
 				CNFDirectory);
