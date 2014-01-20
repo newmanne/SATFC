66f20f648f1d9b8722532e59cecbf896b6ca17a7
afrechet <afrechet@paxos.cs.ubc.ca>
2014-01-20 12:57:10 -0800

    Adjusted SATFC options classes.
    Former-commit-id: 9288f3a4423ebd96595b7c6b8afd3538350df390 [formerly 4186f0f74891867924cb77d5508cc5551b787a8c]
    
    Former-commit-id: 151c43d912f24f087dabfe6bcebbf91d16810b78

--- src/ca/ubc/cs/beta/stationpacking/execution/parameters/solver/SATFCParameters.java
+++ src/ca/ubc/cs/beta/stationpacking/execution/parameters/solver/SATFCParameters.java
@@ -1,26 +1,17 @@
 package ca.ubc.cs.beta.stationpacking.execution.parameters.solver;
 
 import ca.ubc.cs.beta.aclib.logging.ComplexLoggingOptions;
-import ca.ubc.cs.beta.aclib.logging.LoggingOptions;
 import ca.ubc.cs.beta.aclib.misc.options.UsageTextField;
 import ca.ubc.cs.beta.aclib.options.AbstractOptions;
-import ca.ubc.cs.beta.stationpacking.execution.parameters.base.StationPackingQuestionParameters;
 
-import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParametersDelegate;
 
-@UsageTextField(title="SATFC Parameters",description="Parameters needed to execute SATFC on a single instance.")
+@UsageTextField(title="SATFC Parameters",description="Parameters needed to instantiate a SATFC.")
 public class SATFCParameters extends AbstractOptions {
 
 	@ParametersDelegate
 	public SolverManagerParameters SolverManagerParameters = new SolverManagerParameters();
 	
 	@ParametersDelegate
-	public StationPackingQuestionParameters QuestionParameters = new StationPackingQuestionParameters();
-	
-	@ParametersDelegate
-	public LoggingOptions LoggingOptions = new ComplexLoggingOptions();
-	
-	@Parameter(names = "-WORKDIR", description = "Working directory (especially where to find problem data).")
-	public String WorkDirectory = "";
+	public ComplexLoggingOptions LoggingOptions = new ComplexLoggingOptions();
 }
