1e29f6283afcccd1bf38e851e68f0cce07d35462
afrechet <afrechet@paxos.cs.ubc.ca>
2014-03-07 20:49:14 -0800

    Started facade parameters.
    Former-commit-id: 9b052e39371065538d570a99e216e069e286e1e0 [formerly 101adaa29d32aec96510430344f9259e6a86ce87]
    
    Former-commit-id: 3f2855703267074028e04dfa3f6cc1fba654425e

---
+++ src/ca/ubc/cs/beta/stationpacking/execution/SATFCFacadeExecutor.java
@@ -0,0 +1,13 @@
+package ca.ubc.cs.beta.stationpacking.execution;
+
+public class SATFCFacadeExecutor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
