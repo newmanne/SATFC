56a37c31fec1e305f3b0f091c44bcb237a76ec73
afrechet <afrechet@paxos.cs.ubc.ca>
2014-03-07 21:13:25 -0800

    Tested SATFC facade.
    Former-commit-id: a4f7081c3776ebc8b9e236db25a185394fc048c5 [formerly 7809e9fcab96e0c2f4a2bfe8b618e246e14414bd]
    
    Former-commit-id: 2adde50c6e4a8db2ad1218c3bf0e8b44d00c4928

---
+++ src/ca/ubc/cs/beta/stationpacking/execution/parameters/SATFCFacadeParameters.java
@@ -0,0 +1,26 @@
+package ca.ubc.cs.beta.stationpacking.execution.parameters;
+
+import ca.ubc.cs.beta.aclib.logging.ComplexLoggingOptions;
+import ca.ubc.cs.beta.aclib.misc.options.UsageTextField;
+import ca.ubc.cs.beta.aclib.options.AbstractOptions;
+import ca.ubc.cs.beta.stationpacking.execution.parameters.solver.base.InstanceParameters;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParametersDelegate;
+
+@UsageTextField(title="SATFC Facade Parameters",description="Parameters needed to execute SATFC facade on a single instance.")
+public class SATFCFacadeParameters extends AbstractOptions {
+	
+	@ParametersDelegate
+	public InstanceParameters fInstanceParameters = new InstanceParameters();
+	
+	@Parameter(names = "-DATA-FOLDERNAME",description = "station config data folder name", required=true)
+	public String fDataFoldername;
+	
+	@Parameter(names = "-CLASP-LIBRARY",description = "clasp library file", required=true)
+	public String fClaspLibrary;
+	
+	@ParametersDelegate
+	public ComplexLoggingOptions fLoggingOptions = new ComplexLoggingOptions();
+	
+}
