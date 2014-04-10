1d03d2017f2f0645990b21af37893910636457dd
afrechet <afrechet@paxos.cs.ubc.ca>
2014-04-10 13:39:25 -0700

    Fixed a bug with CPU time termination criterion, which would before count elapsed time since the start of the JVM. Reduced verbosity of some logging. Started studying interference graph.
    Former-commit-id: a9635260b955899f61da41c4b9e89870ccbfa729

---
+++ src/ca/ubc/cs/beta/stationpacking/treewidth/GraphStudy.java
@@ -0,0 +1,13 @@
+package ca.ubc.cs.beta.stationpacking.treewidth;
+
+public class GraphStudy {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
