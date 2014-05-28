4282a6d5aae15dab6f20dd2d1ee7ec207a2fc70e
afrechet <afrechet@paxos.cs.ubc.ca>
2014-05-28 09:45:46 -0700

    Modified SATFC TAE to support feasibility problems in additional instance info.
    Former-commit-id: d4795be17af3370851c9cf5072860592ee1d45d1 [formerly bc60cc30eafccc1daeb36d62db8dc08c7a7db4e5]
    
    Former-commit-id: 65bd56e16cec3131a86126717224e202efdaea6a

--- src/ca/ubc/cs/beta/stationpacking/tae/SATFCTargetAlgorithmEvaluatorOptions.java
+++ src/ca/ubc/cs/beta/stationpacking/tae/SATFCTargetAlgorithmEvaluatorOptions.java
@@ -8,16 +8,12 @@ import com.beust.jcommander.Parameter;
 @UsageTextField(title="SATFC Target Algorithm Evaluator Options",description="Options needed to create a SATFC target algorithm evaluator.")
 public class SATFCTargetAlgorithmEvaluatorOptions extends AbstractOptions {
 	
-	@UsageTextField(claimRequired="--station-config-folder")
-	@Parameter(names = "--station-config-folder",description = "where to find station config data folders")
+	@UsageTextField(claimRequired="--satfctae-station-folder")
+	@Parameter(names = "--satfctae-config-folder",description = "where to find station config data folders")
 	public String fStationConfigFolder;
 	
-	@UsageTextField(claimRequired="--instances-folder")
-	@Parameter(names = "--instances-folder",description = "where to look for problem instances")
-	public String fInstancesFolder;
-	
-	@UsageTextField(claimRequired="--library")
-	@Parameter(names = "--library",description = "the location of the necessary (clasp) SAT solver library")
+	@UsageTextField(claimRequired="--satfctae-library")
+	@Parameter(names = "--satfctae-library",description = "the location of the necessary (clasp) SAT solver library")
 	public String fLibrary;
 	
 }
