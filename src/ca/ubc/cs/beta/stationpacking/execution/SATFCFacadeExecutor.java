6d32366b043e0a4f545dacfbf31f93123589c0a0
afrechet <afrechet@paxos.cs.ubc.ca>
2014-06-03 14:10:09 -0700

    Finalized build script. Tweaked clasp library auto-detection. Restructed jar lib folder.
    Former-commit-id: 61c83a33bf9f9ff314654ec6870cdd09798a240d [formerly 7097969a652120d6766505e53799d5c7ba7f221e]
    
    Former-commit-id: 6b85c516f3a21fdc70d47f6c6c012eb4866b2952

--- src/ca/ubc/cs/beta/stationpacking/execution/SATFCFacadeExecutor.java
+++ src/ca/ubc/cs/beta/stationpacking/execution/SATFCFacadeExecutor.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ca.ubc.cs.beta.aeatk.misc.jcommander.JCommanderHelper;
+import ca.ubc.cs.beta.aeatk.misc.returnvalues.AEATKReturnValues;
 import ca.ubc.cs.beta.aeatk.targetalgorithmevaluator.init.TargetAlgorithmEvaluatorLoader;
 import ca.ubc.cs.beta.stationpacking.execution.parameters.SATFCFacadeParameters;
 import ca.ubc.cs.beta.stationpacking.facade.SATFCFacade;
@@ -20,49 +21,66 @@ public class SATFCFacadeExecutor {
 	public static void main(String[] args) {
 		
 		//Parse the command line arguments in a parameter object.
-		SATFCFacadeParameters parameters = new SATFCFacadeParameters();
-		//Check for help
-		try
+		Logger log = null ;
+		try 
 		{
-			JCommanderHelper.parseCheckingForHelpAndVersion(args, parameters,TargetAlgorithmEvaluatorLoader.getAvailableTargetAlgorithmEvaluators());
-		}
-		catch (ParameterException aParameterException)
-		{
-			throw aParameterException;
-		}
-		SATFCFacade.initializeLogging(parameters.fLoggingOptions.logLevel);
-		Logger log = LoggerFactory.getLogger(SATFCFacadeExecutor.class);
-		
-		log.info("Initializing facade.");
-		
-		SATFCFacadeBuilder satfcBuilder = new SATFCFacadeBuilder();
-		
-		String library = parameters.fClaspLibrary;
-		if(library != null)
-		{
-			satfcBuilder.setLibrary(parameters.fClaspLibrary);
-		}
-		satfcBuilder.setInitializeLogging(true);
-		
-		try(SATFCFacade satfc = satfcBuilder.build())
-		{
-			log.info("Solving ...");
-			SATFCResult result = satfc.solve(
-					parameters.fInstanceParameters.getPackingStationIDs(),
-					parameters.fInstanceParameters.getPackingChannels(),
-					parameters.fInstanceParameters.getDomains(),
-					parameters.fInstanceParameters.getPreviousAssignment(),
-					parameters.fInstanceParameters.Cutoff,
-					parameters.fInstanceParameters.Seed,
-					parameters.fDataFoldername);
+			SATFCFacadeParameters parameters = new SATFCFacadeParameters();
+			try 
+			{
+				//Check for help
+				JCommanderHelper.parseCheckingForHelpAndVersion(args, parameters,TargetAlgorithmEvaluatorLoader.getAvailableTargetAlgorithmEvaluators());
+				SATFCFacade.initializeLogging(parameters.fLoggingOptions.logLevel);
+			}
+			finally
+			{
+				log = LoggerFactory.getLogger(SATFCFacadeExecutor.class);
+			}
 			
-			log.info("..done!");
+			log.info("Initializing facade.");
+			SATFCFacadeBuilder satfcBuilder = new SATFCFacadeBuilder();
 			
-			System.out.println(result.getResult());
-			System.out.println(result.getRuntime());
-			System.out.println(result.getWitnessAssignment());
+			String library = parameters.fClaspLibrary;
+			if(library != null)
+			{
+				satfcBuilder.setLibrary(parameters.fClaspLibrary);
+			}
+			satfcBuilder.setInitializeLogging(true);
+			
+			try(SATFCFacade satfc = satfcBuilder.build())
+			{
+				log.info("Solving ...");
+				SATFCResult result = satfc.solve(
+						parameters.fInstanceParameters.getPackingStationIDs(),
+						parameters.fInstanceParameters.getPackingChannels(),
+						parameters.fInstanceParameters.getDomains(),
+						parameters.fInstanceParameters.getPreviousAssignment(),
+						parameters.fInstanceParameters.Cutoff,
+						parameters.fInstanceParameters.Seed,
+						parameters.fDataFoldername);
+				
+				log.info("..done!");
+				
+				System.out.println(result.getResult());
+				System.out.println(result.getRuntime());
+				System.out.println(result.getWitnessAssignment());
+			}
+		} catch (ParameterException e)
+		{
+			log.error("Invalid parameter argument detected ({}).",e.getMessage());
+			System.exit(AEATKReturnValues.PARAMETER_EXCEPTION);
+		} catch (RuntimeException e)
+		{
+			log.error("Runtime exception encountered ({})",e.getMessage());
+			System.exit(AEATKReturnValues.UNCAUGHT_EXCEPTION);
+		}catch(UnsatisfiedLinkError e)
+		{
+			log.error("Couldn't initialize facade, see previous log messages and/or try logging with DEBUG.");
+			System.exit(AEATKReturnValues.UNCAUGHT_EXCEPTION);
+		}catch(Throwable t)
+		{
+			log.error("Throwable encountered ({})",t.getMessage());
+			System.exit(AEATKReturnValues.UNCAUGHT_EXCEPTION);
 		}
-
 	}
 
 }
--- src/ca/ubc/cs/beta/stationpacking/tae/SATFCTargetAlgorithmEvaluatorOptions.java
+++ src/ca/ubc/cs/beta/stationpacking/tae/SATFCTargetAlgorithmEvaluatorOptions.java
@@ -5,15 +5,13 @@ import ca.ubc.cs.beta.aeatk.options.AbstractOptions;
 
 import com.beust.jcommander.Parameter;
 
-@UsageTextField(title="SATFC Target Algorithm Evaluator Options",description="Options needed to create a SATFC target algorithm evaluator.")
+@UsageTextField(title="SATFC Target Algorithm Evaluator Options",description="Options needed to create a SATFC target algorithm evaluator.",claimRequired="--satfctae-station-folder,--satfctae-library")
 public class SATFCTargetAlgorithmEvaluatorOptions extends AbstractOptions {
-	
-	@UsageTextField(claimRequired="--satfctae-station-folder")
+
 	@Parameter(names = "--satfctae-config-folder",description = "where to find station config data folders")
 	public String fStationConfigFolder;
 	
-	@UsageTextField(claimRequired="--satfctae-library")
-	@Parameter(names = "--satfctae-library",description = "the location of the necessary (clasp) SAT solver library")
+	@Parameter(names = {"--satfctae-library","-CLASP-LIBRARY"},description = "the location of the necessary (clasp) SAT solver library")
 	public String fLibrary;
 	
 }
