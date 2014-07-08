c62a76061a82b2e87c3f8da1f0b076e1b8190b88
afrechet <afrechet@paxos.cs.ubc.ca>
2014-07-08 15:20:18 -0700

    Removed unused solvers from SATsolvers.
    Former-commit-id: 353f84860694b076276d6383ba74463b036df0d1 [formerly eae4bc55e8e9e3c8d6f68d30647cf755a392bc3d]
    
    Former-commit-id: 6edeaabd3320e6e7fa5cf96ece33cfc1e33580f3

--- SATsolvers/glucose/glucosex64/full_glucose.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/ubc/cs/home/a/afrechet/arrow-space/workspace/StationPacking/SATsolvers/glucose/glucosex64/glucose.sh /ubc/cs/home/a/afrechet/arrow-space/workspace/StationPacking/SATsolvers/glucose/glucosex64/glucose_2.1/SatELite_release /ubc/cs/home/a/afrechet/arrow-space/workspace/StationPacking/SATsolvers/glucose/glucosex64/glucose_2.1/glucose_static $1
-
-
--- SATsolvers/glueminisat/glueminisat-2.2.7/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export MROOT=$PWD/code
-
-rm -rf binary/glueminisat-simp
-make -C code/core clean 
-make -C code/simp clean 
- 
