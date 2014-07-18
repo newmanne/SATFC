18e22b22ddd1ca7e3b110d00584f39ac7454568a
Ricky Chen <rickychen92@gmail.com>
2014-07-18 16:55:20 -0700

    rename to SwitchFC
    Former-commit-id: a3f882a6bf6a487b2d830b3d5b2b807c9e4e0622 [formerly 6233b5dadebfbb2d95bfbd072f8f94e63dfe90f4]
    
    Former-commit-id: de956863c3360751ae559b3182e972ca8cfc512d

--- src/ca/ubc/cs/beta/stationpacking/tae/switchfc/SwitchFCTargetAlgorithmEvaluatorOptions.java
+++ src/ca/ubc/cs/beta/stationpacking/tae/switchfc/SwitchFCTargetAlgorithmEvaluatorOptions.java
@@ -1,10 +1,10 @@
-package ca.ubc.cs.beta.stationpacking.tae.cli;
+package ca.ubc.cs.beta.stationpacking.tae.switchfc;
 
 import com.beust.jcommander.Parameter;
 
 import ca.ubc.cs.beta.aeatk.options.AbstractOptions;
 
-public class SatfcCliTargetAlgorithmEvaluatorOptions extends AbstractOptions {
+public class SwitchFCTargetAlgorithmEvaluatorOptions extends AbstractOptions {
 
     @Parameter(names = {"--switchtae-tmpdir"},description = "Temporary directory to store CNF files.",required=true)
     public String fTmpDir;
