3b1b0d19c885b69271bc0d67293191bc928d799f
Ricky Chen <rickychen92@gmail.com>
2014-07-22 15:54:32 -0700

    Added sha1 hash method to CNF object.
    Former-commit-id: 1a0e0e88c0eb6a348547503019e051c127fdfa8c [formerly c9b8bfa2e3e95c9fddf0aba4916813af8bc12605]
    
    Former-commit-id: eb2530f4d64f7769bd098155bb812de0fb57c615

--- src/ca/ubc/cs/beta/stationpacking/solvers/sat/base/CNF.java
+++ src/ca/ubc/cs/beta/stationpacking/solvers/sat/base/CNF.java
@@ -1,11 +1,15 @@
 package ca.ubc.cs.beta.stationpacking.solvers.sat.base;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
 
 /**
@@ -13,164 +17,177 @@ import org.apache.commons.lang3.StringUtils;
  * @author afrechet
  */
 public class CNF implements Collection<Clause>{
-	
-	private final Collection<Clause> fClauses;
-	
-	public CNF()
-	{
-		fClauses = new ArrayDeque<Clause>();
-		//fClauses = new HashSet<Clause>();
-	}
-	
-	/**
-	 * Builds and returns the <a href="http://fairmut3x.wordpress.com/2011/07/29/cnf-conjunctive-normal-form-dimacs-format-explained/">DIMACS</a> string representation of the CNF.
-	 * @param aComments - the comments to add at the beginning of the CNF, if any.
-	 * @return the DIMACS string representation of the CNF.
-	 */
-	public String toDIMACS(String[] aComments)
-	{
-		StringBuilder aStringBuilder = new StringBuilder();
-		
-		int aNumClauses = fClauses.size();
-		long aMaxVariable = 0;
-		
-		for(Clause aClause : fClauses)
-		{
-			ArrayList<String> aLitteralStrings = new ArrayList<String>();
-			
-			for(Literal aLitteral : aClause)
-			{
-				if(aLitteral.getVariable()<=0)
-				{
-					throw new IllegalArgumentException("Cannot transform to DIMACS a CNF that has a litteral with variable value <= 0 (clause: "+aClause.toString()+").");
-				}
-				else if(aLitteral.getVariable()>aMaxVariable)
-				{
-					aMaxVariable = aLitteral.getVariable();
-				}
-				aLitteralStrings.add((aLitteral.getSign() ? "" : "-") + Long.toString(aLitteral.getVariable()));
-			}
-			
-			aStringBuilder.append(StringUtils.join(aLitteralStrings," ")+" 0\n");
-		}
-		
-		aStringBuilder.insert(0, "p cnf "+aMaxVariable+" "+aNumClauses+"\n");
-		
-		if (aComments != null)
-		{
-			for(int i=aComments.length-1;i>=0;i--)
-			{
-				aStringBuilder.insert(0, "c "+aComments[i].trim()+"\n");
-			}
-		}
-		
-		return aStringBuilder.toString();
-	}
-	
-	/**
-	 * @return all the variables present in the CNF.
-	 */
-	public Collection<Long> getVariables()
-	{
-		Collection<Long> aVariables = new HashSet<Long>();
-		
-		for(Clause aClause : fClauses)
-		{
-			for(Literal aLitteral : aClause)
-			{
-				aVariables.add(aLitteral.getVariable());
-			}
-		}
-		
-		return aVariables;
-	}
-	
-	@Override
-	public String toString()
-	{
-		ArrayDeque<String> aClauseStrings = new ArrayDeque<String>();
-		for(Clause aClause : fClauses)
-		{
-			aClauseStrings.add("("+aClause.toString()+")");
-		}
-		return StringUtils.join(aClauseStrings," ^ ");
-	}
 
-	@Override
-	public int size() {
-		return fClauses.size();
-	}
+    private final Collection<Clause> fClauses;
 
-	@Override
-	public boolean isEmpty() {
-		return fClauses.isEmpty();
-	}
+    public CNF()
+    {
+        fClauses = new ArrayDeque<Clause>();
+        //fClauses = new HashSet<Clause>();
+    }
 
-	@Override
-	public boolean contains(Object o) {
-		return fClauses.contains(o);
-	}
+    /**
+     * Builds and returns the <a href="http://fairmut3x.wordpress.com/2011/07/29/cnf-conjunctive-normal-form-dimacs-format-explained/">DIMACS</a> string representation of the CNF.
+     * @param aComments - the comments to add at the beginning of the CNF, if any.
+     * @return the DIMACS string representation of the CNF.
+     */
+    public String toDIMACS(String[] aComments)
+    {
+        StringBuilder aStringBuilder = new StringBuilder();
 
-	@Override
-	public Iterator<Clause> iterator() {
-		return fClauses.iterator();
-	}
+        int aNumClauses = fClauses.size();
+        long aMaxVariable = 0;
 
-	@Override
-	public Object[] toArray() {
-		return fClauses.toArray();
-	}
+        for(Clause aClause : fClauses)
+        {
+            ArrayList<String> aLitteralStrings = new ArrayList<String>();
 
-	@Override
-	public <T> T[] toArray(T[] a) {
-		return fClauses.toArray(a);
-	}
+            for(Literal aLitteral : aClause)
+            {
+                if(aLitteral.getVariable()<=0)
+                {
+                    throw new IllegalArgumentException("Cannot transform to DIMACS a CNF that has a litteral with variable value <= 0 (clause: "+aClause.toString()+").");
+                }
+                else if(aLitteral.getVariable()>aMaxVariable)
+                {
+                    aMaxVariable = aLitteral.getVariable();
+                }
+                aLitteralStrings.add((aLitteral.getSign() ? "" : "-") + Long.toString(aLitteral.getVariable()));
+            }
 
-	@Override
-	public boolean add(Clause e) {
-		
-		if(e==null)
-		{
-			throw new IllegalArgumentException("Cannot add a null clause to a CNF.");
-		}
-		
-		return fClauses.add(e);
-	}
+            aStringBuilder.append(StringUtils.join(aLitteralStrings," ")+" 0\n");
+        }
 
-	@Override
-	public boolean remove(Object o) {
-		return fClauses.remove(o);
-	}
+        aStringBuilder.insert(0, "p cnf "+aMaxVariable+" "+aNumClauses+"\n");
 
-	@Override
-	public boolean containsAll(Collection<?> c) {
-		return fClauses.containsAll(c);
-	}
+        if (aComments != null)
+        {
+            for(int i=aComments.length-1;i>=0;i--)
+            {
+                aStringBuilder.insert(0, "c "+aComments[i].trim()+"\n");
+            }
+        }
 
-	@Override
-	public boolean addAll(Collection<? extends Clause> c) {
-		
-		if(c.contains(null))
-		{
-			throw new IllegalArgumentException("Cannot add a null clause to a CNF.");
-		}
-		
-		return fClauses.addAll(c);
-	}
+        return aStringBuilder.toString();
+    }
 
-	@Override
-	public boolean retainAll(Collection<?> c) {
-		return fClauses.retainAll(c);
-	}
+    /**
+     * @return all the variables present in the CNF.
+     */
+    public Collection<Long> getVariables()
+    {
+        Collection<Long> aVariables = new HashSet<Long>();
 
-	@Override
-	public boolean removeAll(Collection<?> c) {
-		return fClauses.remove(c);
-	}
+        for(Clause aClause : fClauses)
+        {
+            for(Literal aLitteral : aClause)
+            {
+                aVariables.add(aLitteral.getVariable());
+            }
+        }
 
-	@Override
-	public void clear() {
-		fClauses.clear();
-	}
+        return aVariables;
+    }
+
+    @Override
+    public String toString()
+    {
+        ArrayDeque<String> aClauseStrings = new ArrayDeque<String>();
+        for(Clause aClause : fClauses)
+        {
+            aClauseStrings.add("("+aClause.toString()+")");
+        }
+        return StringUtils.join(aClauseStrings," ^ ");
+    }
+
+    public String getHashString() {
+        String aString = this.toString();
+        MessageDigest aDigest = DigestUtils.getSha1Digest();
+        try {
+            byte[] aResult = aDigest.digest(aString.getBytes("UTF-8"));
+            String aResultString = new String(Hex.encodeHex(aResult));  
+            return aResultString;
+        }
+        catch (UnsupportedEncodingException e) {
+            throw new IllegalStateException("Could not encode cnf with sha1 hash.", e);
+        }
+    }
+
+    @Override
+    public int size() {
+        return fClauses.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return fClauses.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return fClauses.contains(o);
+    }
+
+    @Override
+    public Iterator<Clause> iterator() {
+        return fClauses.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return fClauses.toArray();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return fClauses.toArray(a);
+    }
+
+    @Override
+    public boolean add(Clause e) {
+
+        if(e==null)
+        {
+            throw new IllegalArgumentException("Cannot add a null clause to a CNF.");
+        }
+
+        return fClauses.add(e);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return fClauses.remove(o);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return fClauses.containsAll(c);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Clause> c) {
+
+        if(c.contains(null))
+        {
+            throw new IllegalArgumentException("Cannot add a null clause to a CNF.");
+        }
+
+        return fClauses.addAll(c);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return fClauses.retainAll(c);
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return fClauses.remove(c);
+    }
+
+    @Override
+    public void clear() {
+        fClauses.clear();
+    }
 
 }
