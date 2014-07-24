f31f223f7236e0d7415cb22ac4faf0c1a6a572fc
Ricky Chen <rickychen92@gmail.com>
2014-07-24 14:20:04 -0700

    SWITCHFC TAE now uses methods from EncodedInstanceToCNFConverter for converting encoded string into CNF file.
    Former-commit-id: 9e81c147a871ece74e3b37ae3cd34c48ba18ecc1 [formerly 8ea9a69fbea430ab1fa12994bbcf5c711c028806]
    
    Former-commit-id: 863382d3992f9c8b0c1e2f21689213ceb591cc65

--- src/ca/ubc/cs/beta/stationpacking/tae/switchfc/SwitchFCTargetAlgorithmEvaluator.java
+++ src/ca/ubc/cs/beta/stationpacking/tae/switchfc/SwitchFCTargetAlgorithmEvaluator.java
@@ -2,23 +2,18 @@ package ca.ubc.cs.beta.stationpacking.tae.switchfc;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.math3.util.Pair;
 import org.slf4j.Logger;
@@ -39,10 +34,9 @@ import ca.ubc.cs.beta.aeatk.targetalgorithmevaluator.base.cli.CommandLineTargetA
 import ca.ubc.cs.beta.stationpacking.base.Station;
 import ca.ubc.cs.beta.stationpacking.base.StationPackingInstance;
 import ca.ubc.cs.beta.stationpacking.datamanagers.constraints.IConstraintManager;
-import ca.ubc.cs.beta.stationpacking.datamanagers.stations.IStationManager;
+import ca.ubc.cs.beta.stationpacking.execution.EncodedInstanceToCNFConverter;
 import ca.ubc.cs.beta.stationpacking.facade.datamanager.data.DataManager;
 import ca.ubc.cs.beta.stationpacking.facade.datamanager.data.ManagerBundle;
-import ca.ubc.cs.beta.stationpacking.solvers.sat.base.CNF;
 import ca.ubc.cs.beta.stationpacking.solvers.sat.cnfencoder.ISATDecoder;
 import ca.ubc.cs.beta.stationpacking.solvers.sat.cnfencoder.ISATEncoder;
 import ca.ubc.cs.beta.stationpacking.solvers.sat.cnfencoder.SATCompressor;
@@ -61,7 +55,7 @@ public class SwitchFCTargetAlgorithmEvaluator extends AbstractSyncTargetAlgorith
     private final DataManager fDataManager;
     private final Map<ManagerBundle,ISATEncoder> fSATencoders;
 
-    private final String fInterferencesConfigFolderDir;
+    private final String fInterferencesConfigFolderDirname;
     private final String fTmpDirname;
 
     SwitchFCTargetAlgorithmEvaluator(SATFCTargetAlgorithmEvaluator aSatfcTae, CommandLineTargetAlgorithmEvaluator aCliTae, String aInterferencesConfigFolderDir, String aTmpDirname) {
@@ -78,7 +72,7 @@ public class SwitchFCTargetAlgorithmEvaluator extends AbstractSyncTargetAlgorith
         fDataManager = new DataManager();
         fSATencoders = new HashMap<ManagerBundle,ISATEncoder>();
 
-        fInterferencesConfigFolderDir = aInterferencesConfigFolderDir;
+        fInterferencesConfigFolderDirname = aInterferencesConfigFolderDir;
 
         File aTmpDir = new File(aTmpDirname);        
 
@@ -142,9 +136,22 @@ public class SwitchFCTargetAlgorithmEvaluator extends AbstractSyncTargetAlgorith
                 String encodedInstanceString = runConfig.getProblemInstanceSeedPair().getProblemInstance().getInstanceSpecificInformation();
 
                 // transform problem instance to CLI-friendly
+                
+                Pair<StationPackingInstance, String> decodedInstanceAndConfigFoldername;
+                
+                try {
+                    decodedInstanceAndConfigFoldername = EncodedInstanceToCNFConverter.getInstanceFromSQLString(encodedInstanceString, fInterferencesConfigFolderDirname, fDataManager);
+                } catch (IllegalArgumentException e) {
+                    log.error("Failed to parse encoded instance string: " + encodedInstanceString, e);
+                    throw new IllegalStateException("Instance specific information of run config " + runConfig + " parsing failed.", e);
+                }
 
-                Pair<CNF, ISATDecoder> cnf = createCNFfromInstanceString(encodedInstanceString);
-                String cnfFilename = printCNFtoFile(fTmpDirname, cnf);
+                String cnfFilename = fTmpDirname+ File.separator +decodedInstanceAndConfigFoldername.getFirst().getHashString()+".cnf";
+                
+                ISATEncoder SATencoder = getSATEncoder(decodedInstanceAndConfigFoldername.getSecond());
+                
+                ISATDecoder SATdecoder = EncodedInstanceToCNFConverter.encodeInstanceToCNFFile(
+                        decodedInstanceAndConfigFoldername.getFirst(), cnfFilename, SATencoder, new String[]{});
 
                 ProblemInstanceSeedPair transformedProblemInstance = new ProblemInstanceSeedPair(
                         new ProblemInstance(cnfFilename), runConfig.getProblemInstanceSeedPair().getSeed());
@@ -153,7 +160,7 @@ public class SwitchFCTargetAlgorithmEvaluator extends AbstractSyncTargetAlgorith
                         transformedProblemInstance, runConfig.getParameterConfiguration(), runConfig.getAlgorithmExecutionConfiguration());                
 
                 transformedToOriginalCliConfigurationsMapping.put(transformedCliRunConfig, runConfig);
-                transformedCliConfigurationsToCNFDecoderMapping.put(transformedCliRunConfig, cnf.getValue());
+                transformedCliConfigurationsToCNFDecoderMapping.put(transformedCliRunConfig, SATdecoder);
             }
         }
 
@@ -444,110 +451,8 @@ public class SwitchFCTargetAlgorithmEvaluator extends AbstractSyncTargetAlgorith
 
         return parsedResults;
     }
-
-
-    /**
-     * Converts an encoded instance string to a CNF, then writes to a file in the given output folder.
-     * @return Location of stored .cnf file
-     */
-    private Pair<CNF, ISATDecoder> createCNFfromInstanceString(String encodedInstanceString) {
-        /*
-         * Get data from instance.
-         */
-        Map<Integer,Set<Integer>> stationID_domains = new HashMap<Integer,Set<Integer>>();
-        Map<Integer,Integer> previous_assignmentID = new HashMap<Integer,Integer>();
-        String configFoldername;
-
-        String[] encoded_instance_parts = encodedInstanceString.split("_");
-
-        if(encoded_instance_parts.length == 0)
-        {
-            throw new IllegalArgumentException("Unparseable encoded instance string \""+encodedInstanceString+"\".");
-        }
-
-        //Get the config folder name.
-        configFoldername = fInterferencesConfigFolderDir + File.separator + encoded_instance_parts[0];
-
-        //Get problem info.
-        for(int i=1;i<encoded_instance_parts.length;i++)
-        {
-            Integer station;
-            Integer previousChannel;
-            Set<Integer> domain;
-
-            String station_info_string = encoded_instance_parts[i];
-            String[] station_info_parts = station_info_string.split(";");
-
-            String station_string = station_info_parts[0];
-            if(isInteger(station_string))
-            {
-                station = Integer.parseInt(station_string);
-            }
-            else
-            {
-                throw new IllegalArgumentException("Unparseable station info \""+station_info_string+"\" (station ID "+station_string+" is not an integer).");
-            }
-
-            String previous_channel_string = station_info_parts[1];
-            if(isInteger(previous_channel_string))
-            {
-                previousChannel = Integer.parseInt(previous_channel_string);
-                if(previousChannel <= 0)
-                {
-                    previousChannel = null;
-                }
-            }
-            else
-            {
-                throw new IllegalArgumentException("Unparseable station info \""+station_info_string+"\" (previous channel "+previous_channel_string+" is not an integer).");
-            }
-
-
-            domain = new HashSet<Integer>();
-            String channels_string = station_info_parts[2];
-            String[] channels_parts = channels_string.split(",");
-            for(String channel_string : channels_parts)
-            {
-                if(isInteger(channel_string))
-                {
-                    domain.add(Integer.parseInt(channel_string));
-                }
-                else
-                {
-                    throw new IllegalArgumentException("Unparseable station info \""+station_info_string+"\" (domain channel "+channel_string+" is not an integer).");
-                }
-            }
-
-
-            stationID_domains.put(station, domain);
-            if(previousChannel != null)
-            {
-                previous_assignmentID.put(station, previousChannel);
-            }
-        }
-
-        /*
-         * Validate instance data.
-         */
-        File config_folder = new File(configFoldername);
-        if(!config_folder.exists())
-        {
-            throw new IllegalArgumentException("Encoded instance's interference config folder \""+configFoldername+"\" does not exist.");
-        }
-        else if(!config_folder.isDirectory())
-        {
-            throw new IllegalArgumentException("Encoded instance's interference config folder \""+configFoldername+"\" is not a directory.");
-        }
-
-        if(!stationID_domains.keySet().containsAll(previous_assignmentID.keySet()))
-        {
-            log.warn("Encoded instance's previous assignment contains stations not in the indicated domains.");
-        }
-
-        /*
-         * Construct station packing instances. 
-         */
-
+    
+    private ISATEncoder getSATEncoder(String configFoldername) {
         ManagerBundle data_bundle;
         try {
             data_bundle = fDataManager.getData(configFoldername);
@@ -555,50 +460,9 @@ public class SwitchFCTargetAlgorithmEvaluator extends AbstractSyncTargetAlgorith
             e.printStackTrace();
             throw new IllegalArgumentException("Could not load interference data from \""+configFoldername+"\".");
         }
-        IStationManager station_manager = data_bundle.getStationManager();
+        
         IConstraintManager constraint_manager = data_bundle.getConstraintManager();
-
-        Map<Station,Set<Integer>> domains = new HashMap<Station,Set<Integer>>();
-        for(Entry<Integer,Set<Integer>> stationID_domains_entry : stationID_domains.entrySet())
-        {
-            Integer stationID = stationID_domains_entry.getKey();
-            Set<Integer> domain = stationID_domains_entry.getValue();
-
-            Station station = station_manager.getStationfromID(stationID);
-
-            Set<Integer> validDomain = station_manager.getDomain(station);
-            if(!validDomain.containsAll(domain))
-            {
-                //log.warn("Domain {} of station {} does not contain all stations specified in problem domain {}.",truedomain,stationID,domain);
-            }
-
-            domains.put(station, Sets.intersection(domain, validDomain));
-        }
-
-        Map<Station,Integer> previous_assignment = new HashMap<Station,Integer>();
-        for(Entry<Integer,Integer> previous_assignmentID_entry : previous_assignmentID.entrySet())
-        {
-            Integer stationID = previous_assignmentID_entry.getKey();
-            Integer previous_channel = previous_assignmentID_entry.getValue();
-
-            Station station = station_manager.getStationfromID(stationID);
-
-            Set<Integer> truedomain = station_manager.getDomain(station);
-            if(!truedomain.contains(previous_channel))
-            {
-                log.warn("Domain {} of station {} does not contain previous assigned channel {}.",truedomain,stationID,previous_channel);
-            }
-
-            previous_assignment.put(station, previous_channel);
-
-        }
-
-        StationPackingInstance instance = new StationPackingInstance(domains,previous_assignment);
-
-
-        /*
-         * Encode instance into CNF.
-         */
+        
         ISATEncoder SATencoder;
         if(fSATencoders.containsKey(data_bundle))
         {
@@ -609,32 +473,8 @@ public class SwitchFCTargetAlgorithmEvaluator extends AbstractSyncTargetAlgorith
             SATencoder = new SATCompressor(constraint_manager);
             fSATencoders.put(data_bundle, SATencoder);
         }
-
-        return SATencoder.encode(instance);
-    }
-
-    private String printCNFtoFile(String outputFoldername, Pair<CNF, ISATDecoder> encoding) {
-        CNF cnf = encoding.getKey();
-
-        String aCNFFilename = outputFoldername+ File.separator +cnf.getHashString()+".cnf";
-
-        try {
-            FileUtils.writeStringToFile(new File(aCNFFilename), cnf.toDIMACS(new String[0]));
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new IllegalStateException("Could not write CNF to file.");
-        }
-
-        return aCNFFilename;
-    }
-
-    private static boolean isInteger(String s) {
-        try { 
-            Integer.parseInt(s); 
-        } catch(NumberFormatException e) { 
-            return false; 
-        }
-        return true;
+        
+        return SATencoder;
     }
 
 }
