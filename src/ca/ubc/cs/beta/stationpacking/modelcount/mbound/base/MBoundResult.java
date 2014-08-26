6b154ab090bbfcfa4aad3ff11f26aba95e72a0ad
Ricky Chen <rickychen92@gmail.com>
2014-08-26 16:35:37 -0700

    Implement basic station packing model count using MBound
    Former-commit-id: f2e2c7b56ffc2f129f2e1b21e09fd2f38eb6eee7 [formerly 1567c7985e8e8ffd6915b2865b0a76082989cf0f]
    
    Former-commit-id: 1cf9fbc042259de7d3dc710744fb29d63cb7ac5d

--- src/ca/ubc/cs/beta/stationpacking/modelcount/mbound/base/MBoundResult.java
+++ src/ca/ubc/cs/beta/stationpacking/modelcount/mbound/base/MBoundResult.java
@@ -1,7 +1,7 @@
 package ca.ubc.cs.beta.stationpacking.modelcount.mbound.base;
 
 public class MBoundResult {
-    
+
     public enum MBoundResultType {
         UPPERBOUND, LOWERBOUND, FAILURE
     }
@@ -9,39 +9,48 @@ public class MBoundResult {
     private final MBoundResultType fResultType;
     private final Double fBound;
     private final Double fErrorProbability;
-    
+
     public MBoundResult(MBoundResultType aResultType, Double aBound, Double aErrorProbability) {
-        
+
         if (aResultType == null) throw new IllegalArgumentException("Result Type cannot be null.");
-        
+
         fResultType = aResultType;
-        
+
         if (aResultType.equals(MBoundResultType.FAILURE)) {
-            
+
             fBound = Double.NaN;
             fErrorProbability = Double.NaN;
-            
+
         } else {
-            
+
             if (aBound == null) throw new IllegalArgumentException("Bound cannot be null.");
             fBound = new Double(aBound);
-            
+
             if (aErrorProbability > 1 || aErrorProbability < 0) throw new IllegalArgumentException("Error probability must be betwee 0 and 1, inclusive.");
             fErrorProbability = aErrorProbability;
-            
+
         }
     }
-    
+
     public MBoundResultType getResultType() {
         return fResultType;
     }
-    
+
     public Double getBound() {
         return fBound;
     }
-    
+
     public Double getProbabilityOfError() {
         return fErrorProbability;
     }
-    
+
+    @Override
+    public String toString() {
+        return "MBoundResult("
+                +fResultType+","
+                +"Bound:"+fBound+","
+                +"ErrorProbability:"+fErrorProbability
+                +")";
+    }
+
 }
