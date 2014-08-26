4114735870f3c401aefb3f228b94646285246fd6
Ricky Chen <rickychen92@gmail.com>
2014-08-26 15:10:45 -0700

    An error probability is returnen in the mbound result as well.
    Former-commit-id: 02b2043b1c986e0a5a4b5bf761a30abf5d2b8137 [formerly d99d4fa67b940a5810ffe9a37f297ee12ab34bf5]
    
    Former-commit-id: 86558e84ed5a2731a442af8e2a6dc3e046f1eb1c

--- src/ca/ubc/cs/beta/stationpacking/modelcount/mbound/base/MBoundResult.java
+++ src/ca/ubc/cs/beta/stationpacking/modelcount/mbound/base/MBoundResult.java
@@ -8,18 +8,27 @@ public class MBoundResult {
 
     private final MBoundResultType fResultType;
     private final Double fBound;
+    private final Double fErrorProbability;
     
-    public MBoundResult(MBoundResultType aResultType, Double aBound) {
+    public MBoundResult(MBoundResultType aResultType, Double aBound, Double aErrorProbability) {
         
         if (aResultType == null) throw new IllegalArgumentException("Result Type cannot be null.");
         
         fResultType = aResultType;
         
         if (aResultType.equals(MBoundResultType.FAILURE)) {
+            
             fBound = Double.NaN;
+            fErrorProbability = Double.NaN;
+            
         } else {
+            
             if (aBound == null) throw new IllegalArgumentException("Bound cannot be null.");
             fBound = new Double(aBound);
+            
+            if (aErrorProbability > 1 || aErrorProbability < 0) throw new IllegalArgumentException("Error probability must be betwee 0 and 1, inclusive.");
+            fErrorProbability = aErrorProbability;
+            
         }
     }
     
@@ -29,6 +38,10 @@ public class MBoundResult {
     
     public Double getBound() {
         return fBound;
+    }
+    
+    public Double getProbabilityOfError() {
+        return fErrorProbability;
     }
     
 }
