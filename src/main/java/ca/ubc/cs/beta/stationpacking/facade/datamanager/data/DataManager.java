172db67c83c9c0b5c3705020301a69394015dff6
afrechet <afrechet@cs.ubc.ca>
2014-12-04 13:12:57 -0800

    Changed constraint data manager to unabridged format.

--- src/main/java/ca/ubc/cs/beta/stationpacking/facade/datamanager/data/DataManager.java
+++ src/main/java/ca/ubc/cs/beta/stationpacking/facade/datamanager/data/DataManager.java
@@ -25,8 +25,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 
-import ca.ubc.cs.beta.stationpacking.datamanagers.constraints.ChannelSpecificConstraintManager;
 import ca.ubc.cs.beta.stationpacking.datamanagers.constraints.IConstraintManager;
+import ca.ubc.cs.beta.stationpacking.datamanagers.constraints.UnabridgedFormatConstraintManager;
 import ca.ubc.cs.beta.stationpacking.datamanagers.stations.DomainStationManager;
 import ca.ubc.cs.beta.stationpacking.datamanagers.stations.IStationManager;
 
@@ -73,10 +73,8 @@ public class DataManager {
 		else
 		{
 			IStationManager stationManager = new DomainStationManager(path+DOMAIN_FILE);
-			
-			
-			IConstraintManager constraintManager = new ChannelSpecificConstraintManager(stationManager, path+INTERFERENCES_FILE);
-			//IConstraintManager constraintManager = new UnabridgedFormatConstraintManager(stationManager, path+INTERFERENCES_FILE);
+
+			IConstraintManager constraintManager = new UnabridgedFormatConstraintManager(stationManager, path+INTERFERENCES_FILE);
 			fData.put(path, new ManagerBundle(stationManager, constraintManager));
 			return true;
 		}
