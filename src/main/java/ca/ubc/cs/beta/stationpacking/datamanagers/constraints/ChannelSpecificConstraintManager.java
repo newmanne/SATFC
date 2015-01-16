eab89aa737e98795ef662959f4a676b39f26c011
afrechet <afrechet@paxos.cs.ubc.ca>
2015-01-16 11:23:26 -0800

    Removed logging from channel specific constraint manager.

--- src/main/java/ca/ubc/cs/beta/stationpacking/datamanagers/constraints/ChannelSpecificConstraintManager.java
+++ src/main/java/ca/ubc/cs/beta/stationpacking/datamanagers/constraints/ChannelSpecificConstraintManager.java
@@ -253,7 +253,7 @@ public class ChannelSpecificConstraintManager implements IConstraintManager{
 				//Check if we have already seen station1
 				if(allStations.contains(station1))
 				{
-					log.warn("Station {} is assigned to multiple channels.");
+					//log.warn("Station {} is assigned to multiple channels.");
 					return false;
 				}
 				
@@ -263,7 +263,7 @@ public class ChannelSpecificConstraintManager implements IConstraintManager{
 				{
 					if(coInterferingStations.contains(station2))
 					{
-						log.warn("Station {} and {} share channel {} on which they CO interfere.",station1,station2,channel);
+						//log.warn("Station {} and {} share channel {} on which they CO interfere.",station1,station2,channel);
 						return false;
 					}
 				}
@@ -278,7 +278,7 @@ public class ChannelSpecificConstraintManager implements IConstraintManager{
 					{
 						if(adjInterferingStations.contains(station2))
 						{
-							log.warn("Station {} is on channel {}, and station {} is on channel {}, causing ADJ+1 interference.",station1,channel,station2,channelp1);
+							//log.warn("Station {} is on channel {}, and station {} is on channel {}, causing ADJ+1 interference.",station1,channel,station2,channelp1);
 							return false;
 						}
 					}
