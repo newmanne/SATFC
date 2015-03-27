dd84ab3a8aec384f8b179135535118c283f3ce1e
newmanne <newman.n8@gmail.com>
2015-03-27 11:13:23 -0700

    Move license plugin to root project. Move git related functions to root project. Redo license plugin

--- satfcserver/src/main/java/ca/ubc/cs/beta/stationpacking/webapp/Application.java
+++ satfcserver/src/main/java/ca/ubc/cs/beta/stationpacking/webapp/Application.java
@@ -1,20 +1,20 @@
 /**
- * Copyright 2015, Auctionomics, Alexandre Fréchette, Kevin Leyton-Brown.
+ * Copyright 2015, Auctionomics, Alexandre Fréchette, Neil Newman, Kevin Leyton-Brown.
  *
- * This file is part of satfcserver.
+ * This file is part of SATFC.
  *
- * satfcserver is free software: you can redistribute it and/or modify
+ * SATFC is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * satfcserver is distributed in the hope that it will be useful,
+ * SATFC is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with satfcserver.  If not, see <http://www.gnu.org/licenses/>.
+ * along with SATFC.  If not, see <http://www.gnu.org/licenses/>.
  *
  * For questions, contact us at:
  * afrechet@cs.ubc.ca
