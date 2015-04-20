40f55165382fb29e542f4f1db37a3c7a70ef14d2
newmanne <newman.n8@gmail.com>
2015-04-20 13:23:48 -0700

    Make clasp compilable and runnable on the arrow file system, or in general when libtbb is a local shared library and not installed

--- satfc/src/dist/clasp/compile.sh
+++ satfc/src/dist/clasp/compile.sh
@@ -3,7 +3,7 @@
 
 tar -xzvf libjnaclasp-3.tar.gz
 cd libjnaclasp-3/jna
-./build.sh
+./build.sh $1 $2 # These arguments are TBB_INCLUDE and TBB_LIB. If you have tbb properly installed, these arguments are not necessary
 mkdir -p ../../jna
 mv libjnaclasp.so ../../jna
 cd ../../
---
+++ satfc/src/dist/clasp/compile_arrow.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Compile clasp3 on the arrow file system
+./compile.sh /cs/public/lib/pkg/tbb43_20150316oss/include /cs/public/lib/pkg/tbb43_20150316oss/lib/intel64/gcc4.4
