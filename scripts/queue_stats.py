92dc4f2260d84ee4e6a12ea20ee693913059795a
newmanne <newman.n8@gmail.com>
2015-04-28 11:53:41 -0700

    Add a simple queue stats

---
+++ scripts/queue_stats.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+import redis
+import argparse
+
+# Load up a redis queue with jobs
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--qname', type=str, help="redis queue to delete")
+parser.add_argument('--host', type=str, help="redis host")
+parser.add_argument('--port', type=int, help="redis port")
+args = parser.parse_args()
+
+r = redis.StrictRedis(host=args.host, port=args.port)
+remaining_jobs = r.llen(args.qname)
+processing_jobs = r.llen(args.qname+'_PROCESSING')
+print "There are %d jobs in the queue" % remaining_jobs
+print "There are %d jobs in the processing queue" % processing_jobs
