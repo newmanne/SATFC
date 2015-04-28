03159b2c6921ef2ab16369b05bac63b54181a02a
newmanne <newman.n8@gmail.com>
2015-04-27 21:45:37 -0700

    The most hacked together redis queue reading implementation ever Fix a bug in solvedBy metric, but did so in an unforgivably hacky way that will almost certainly come back to haunt me

--- scripts/load_redis.py
+++ scripts/load_redis.py
@@ -7,11 +7,12 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--qname', type=str, help="redis queue to store jobs in")
 parser.add_argument('--host', type=str, help="redis host")
+parser.add_argument('--srpkfile', type=str, help="file with one srpk per line")
 parser.add_argument('--port', type=int, help="redis port")
 args = parser.parse_args()
 
 r = redis.StrictRedis(host=args.host, port=args.port)
-with open('small.txt', 'r') as srpkfile:
+with open(args.srpkfile, 'r') as srpkfile:
     lines = [line.strip() for line in srpkfile.readlines()]
     for line in lines:
-        r.push('qname', line)
+        r.rpush(args.qname, line)
