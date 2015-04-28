63272222a8ddc67596f52747a44a4935b708f103
newmanne <newman.n8@gmail.com>
2015-04-27 22:27:24 -0700

    add command to reset a redis queue

---
+++ scripts/reset_redis_queue.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+import redis
+import argparse
+
+# Load up a redis queue with jobs
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--qname', type=str, help="redis queue to delete")
+parser.add_argument('--host', type=str, help="redis host")
+parser.add_argument('--port', type=int, help="redis port")
+args = parser.parse_args()
+
+r = redis.StrictRedis(host=args.host, port=args.port)
+r.del(args.qname)
+r.del(args.qname+'_PROCESSING')
