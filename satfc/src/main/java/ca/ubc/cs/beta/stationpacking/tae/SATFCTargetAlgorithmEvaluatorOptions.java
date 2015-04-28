98108d483bafd64857107c15947a602218f26783
afrechet <afrechet@cs.ubc.ca>
2015-04-28 12:21:08 -0700

    Streamlined the SATFC TAE class.

--- satfc/src/main/java/ca/ubc/cs/beta/stationpacking/tae/SATFCTargetAlgorithmEvaluatorOptions.java
+++ satfc/src/main/java/ca/ubc/cs/beta/stationpacking/tae/SATFCTargetAlgorithmEvaluatorOptions.java
@@ -26,7 +26,8 @@ import ca.ubc.cs.beta.aeatk.options.AbstractOptions;
 
 import com.beust.jcommander.Parameter;
 
-@UsageTextField(title="SATFC Target Algorithm Evaluator Options",description="Options needed to create a SATFC target algorithm evaluator.",claimRequired="--satfctae-station-folder,--satfctae-library")
+@UsageTextField(title="SATFC Target Algorithm Evaluator Options",description="Options needed to create a SATFC target algorithm evaluator.",
+claimRequired="--satfctae-config-folder,--satfctae-library")
 public class SATFCTargetAlgorithmEvaluatorOptions extends AbstractOptions {
 
 	@Parameter(names = "--satfctae-config-folder",description = "where to find station config data folders")
