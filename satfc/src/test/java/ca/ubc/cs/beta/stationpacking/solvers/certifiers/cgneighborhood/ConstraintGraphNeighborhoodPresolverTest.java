b787b5558c86601cb4b23a1a9fab4f9c1dbb536f
Paul Cernek <pcernek@cersei.cs.ubc.ca>
2015-04-28 17:11:54 -0700

    Stub out unit test for ConstraintGraphNeighborhoodPresolverTest

---
+++ satfc/src/test/java/ca/ubc/cs/beta/stationpacking/solvers/certifiers/cgneighborhood/ConstraintGraphNeighborhoodPresolverTest.java
@@ -0,0 +1,37 @@
+package ca.ubc.cs.beta.stationpacking.solvers.certifiers.cgneighborhood;
+
+import ca.ubc.cs.beta.stationpacking.base.StationPackingInstance;
+import ca.ubc.cs.beta.stationpacking.solvers.termination.ITerminationCriterion;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Created by pcernek on 4/28/15.
+ */
+public class ConstraintGraphNeighborhoodPresolverTest {
+
+    StationPackingInstance gStationPackingInstance;
+    ITerminationCriterion gTerminationCriterion;
+    long gSeed;
+
+    @Before
+    public void setUp() throws Exception {
+        gStationPackingInstance = mock(StationPackingInstance.class);
+        gTerminationCriterion = mock(ITerminationCriterion.class);
+        gSeed = 17;
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testSolve() throws Exception {
+
+    }
+}
