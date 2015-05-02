18edb50d5927c8aafaee7f41571b13e26d79a94d
Paul Cernek <pcernek@cersei.cs.ubc.ca>
2015-05-01 17:37:00 -0700

    Make GraphBackedConstraintManager not abstract (implement missing methods)

--- satfc/src/test/java/ca/ubc/cs/beta/stationpacking/test/GraphBackedConstraintManager.java
+++ satfc/src/test/java/ca/ubc/cs/beta/stationpacking/test/GraphBackedConstraintManager.java
@@ -1,7 +1,6 @@
 package ca.ubc.cs.beta.stationpacking.test;
 
 import ca.ubc.cs.beta.stationpacking.base.Station;
-import ca.ubc.cs.beta.stationpacking.base.StationPackingInstance;
 import ca.ubc.cs.beta.stationpacking.datamanagers.constraints.IConstraintManager;
 import org.jgrapht.alg.NeighborIndex;
 import org.jgrapht.graph.DefaultEdge;
@@ -10,25 +9,20 @@ import org.jgrapht.graph.SimpleGraph;
 import java.util.*;
 
 /**
- * Created by pcernek on 4/29/15.
+ * A mock ConstraintManager useful for running tests where the focus is on the shape of interference constraint graphs
+ *  between stations.
+ * @author pcernek
  */
-public abstract class GraphBackedConstraintManager implements IConstraintManager {
+public class GraphBackedConstraintManager implements IConstraintManager {
 
     private final NeighborIndex<Station, DefaultEdge> coInterferingStationNeighborIndex;
     private final NeighborIndex<Station, DefaultEdge> adjInterferingStationNeighborIndex;
 
     /**
-     * Treats the station graph as representing both CO and ADJ interference constraints.
-     * @param stationGraph - the graph that represents the configuration of constraints between stations
-     *              that we want to test. Assumes that this graph has been fully configured with all relevant
-     *              edges between nodes.
+     * Creates a constraint manager backed by the given interference graphs.
+     * @param coInterferingStationGraph - the graph representing CO interfering stations.
+     * @param adjInterferingStationGraph - the graph representing ADJ interfering stations.
      */
-    public GraphBackedConstraintManager(SimpleGraph<Station, DefaultEdge> stationGraph) {
-
-        coInterferingStationNeighborIndex = new NeighborIndex<>(stationGraph);
-        adjInterferingStationNeighborIndex = new NeighborIndex<>(stationGraph);
-    }
-
     public GraphBackedConstraintManager(SimpleGraph<Station, DefaultEdge> coInterferingStationGraph,
                                         SimpleGraph<Station, DefaultEdge> adjInterferingStationGraph) {
 
@@ -37,20 +31,62 @@ public abstract class GraphBackedConstraintManager implements IConstraintManager
 
     }
 
-    /**
-     * @param aStation - a (source) station of interest.
-     * @param aChannel - any integer. This parameter is ignored (exists for compatibility with interface).
-     * @return - a set of "conflicting stations" that is guaranteed to correspond to the shape of the graph with
-     *  which this constraint manager was initialized.
-     */
     @Override
     public Set<Station> getCOInterferingStations(Station aStation, int aChannel) {
         return this.coInterferingStationNeighborIndex.neighborsOf(aStation);
     }
 
+
     @Override
     public Set<Station> getADJplusInterferingStations(Station aStation, int aChannel) {
         return this.adjInterferingStationNeighborIndex.neighborsOf(aStation);
+    }
+
+    @Override
+    public boolean isSatisfyingAssignment(Map<Integer, Set<Station>> aAssignment) {
+
+        if (violatesCOconstraints(aAssignment)) return false;
+
+        if (violatesADJConstraints(aAssignment)) return false;
+
+        return true;
+    }
+
+    private boolean violatesADJConstraints(Map<Integer, Set<Station>> aAssignment) {
+        for (Integer channel: aAssignment.keySet()) {
+            for (Station station : aAssignment.get(channel)) {
+
+                Set<Station> stationsOnNextChannel = aAssignment.get(channel + 1);
+
+                // If (channel + 1) is not contained in the assignment, then there can be no adjacency interference
+                if (stationsOnNextChannel == null)
+                    continue;
+
+                for (Station neighbor : adjInterferingStationNeighborIndex.neighborsOf(station))
+                    if (stationsOnNextChannel.contains(neighbor))
+                        return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean violatesCOconstraints(Map<Integer, Set<Station>> aAssignment) {
+        for (Integer channel : aAssignment.keySet()) {
+            Set<Station> stationsOnChannel = aAssignment.get(channel);
+            for (Station station : stationsOnChannel) {
+                for (Station neighbor : coInterferingStationNeighborIndex.neighborsOf(station))
+                    if (stationsOnChannel.contains(neighbor))
+                        return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public String getHashCode() {
+        return this.toString();
     }
 
 }
