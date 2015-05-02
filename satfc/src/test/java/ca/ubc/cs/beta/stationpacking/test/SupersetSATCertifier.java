f50344607b661960d53d28432c4e8a33b563c5c3
Paul Cernek <pcernek@cersei.cs.ubc.ca>
2015-05-01 17:35:02 -0700

    Rename certifier and complete implementation

--- satfc/src/test/java/ca/ubc/cs/beta/stationpacking/test/SupersetSATCertifier.java
+++ satfc/src/test/java/ca/ubc/cs/beta/stationpacking/test/SupersetSATCertifier.java
@@ -2,28 +2,35 @@ package ca.ubc.cs.beta.stationpacking.test;
 
 import ca.ubc.cs.beta.stationpacking.base.Station;
 import ca.ubc.cs.beta.stationpacking.base.StationPackingInstance;
+import ca.ubc.cs.beta.stationpacking.solvers.base.SATResult;
 import ca.ubc.cs.beta.stationpacking.solvers.base.SolverResult;
 import ca.ubc.cs.beta.stationpacking.solvers.certifiers.cgneighborhood.IStationSubsetCertifier;
 import ca.ubc.cs.beta.stationpacking.solvers.termination.ITerminationCriterion;
 
+import java.util.HashMap;
 import java.util.Set;
 
 /**
- * Created by pcernek on 4/29/15.
+ * Returns SAT when all the stations in the instance of a station packing problem are contained in the "toPackStations"
+ *  parameter.
+ * @author pcernek
  */
-public class AlwaysSATCertifier implements IStationSubsetCertifier {
+public class SupersetSATCertifier implements IStationSubsetCertifier {
+
     @Override
     public SolverResult certify(StationPackingInstance aInstance, Set<Station> aToPackStations, ITerminationCriterion aTerminationCriterion, long aSeed) {
-        return null;
+        if (aToPackStations.containsAll(aInstance.getStations()))
+            return new SolverResult(SATResult.SAT, 0, new HashMap<>());
+        return new SolverResult(SATResult.TIMEOUT, 0);
     }
 
     @Override
     public void interrupt() throws UnsupportedOperationException {
-
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void notifyShutdown() {
-
+        throw new UnsupportedOperationException();
     }
 }
