65dc01c997f5289dd854392fb4dfdd796a23e67f
Paul Cernek <ptcernek@gmail.com>
2015-05-05 16:58:14 -0700

    Stub out tests for GraphBackedConstraintManager

---
+++ satfc/src/test/java/ca/ubc/cs/beta/stationpacking/datamanagers/constraints/GraphBackedConstraintManagerTest.java
@@ -0,0 +1,19 @@
+package ca.ubc.cs.beta.stationpacking.datamanagers.constraints;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GraphBackedConstraintManagerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testIsSatisfyingAssignment() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
