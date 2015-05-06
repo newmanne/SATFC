55093f7a1ff2e8ac467a3820c65465a72cc4a968
Paul Cernek <ptcernek@gmail.com>
2015-05-05 17:01:50 -0700

    Finish implementing Presolver unit test

--- satfc/src/test/java/ca/ubc/cs/beta/stationpacking/solvers/certifiers/cgneighborhood/ConstraintGraphNeighborhoodPresolverTest.java
+++ satfc/src/test/java/ca/ubc/cs/beta/stationpacking/solvers/certifiers/cgneighborhood/ConstraintGraphNeighborhoodPresolverTest.java
@@ -2,21 +2,22 @@ package ca.ubc.cs.beta.stationpacking.solvers.certifiers.cgneighborhood;
 
 import ca.ubc.cs.beta.stationpacking.base.Station;
 import ca.ubc.cs.beta.stationpacking.base.StationPackingInstance;
+import ca.ubc.cs.beta.stationpacking.datamanagers.constraints.GraphBackedConstraintManager;
 import ca.ubc.cs.beta.stationpacking.datamanagers.constraints.IConstraintManager;
 import ca.ubc.cs.beta.stationpacking.solvers.base.SATResult;
 import ca.ubc.cs.beta.stationpacking.solvers.base.SolverResult;
 import ca.ubc.cs.beta.stationpacking.solvers.termination.ITerminationCriterion;
-import ca.ubc.cs.beta.stationpacking.test.GraphBackedConstraintManager;
 import ca.ubc.cs.beta.stationpacking.test.SATFCPaths;
 import ca.ubc.cs.beta.stationpacking.test.SimpleGraphBuilder;
 import ca.ubc.cs.beta.stationpacking.test.StationWholeSetSATCertifier;
 
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.*;
 
 import static org.junit.Assert.assertEquals;
@@ -31,16 +32,14 @@ public class ConstraintGraphNeighborhoodPresolverTest {
     private static final int ARBITRARY_CHANNEL = 42;
     private static ITerminationCriterion mockTerminationCriterion;
     private static long arbitrarySeed;
-//    private static List<IStationSubsetCertifier> neverSATCertifier = new ArrayList<>();
 
     SimpleGraph <Station,DefaultEdge> noNeighbors;
     SimpleGraph <Station,DefaultEdge> bigConnectedGraph;
-    SimpleGraph <Station,DefaultEdge> oneRingOfNeighbors;
-//    SimpleGraph <Station,DefaultEdge> hubAndSpoke;
-//    SimpleGraph <Station,DefaultEdge> longChainOfNeighbors;
-//    SimpleGraph <Station,DefaultEdge> shortChainOfNeighbors;
-//    SimpleGraph <Station,DefaultEdge> biPartiteGraph;
-//    SimpleGraph <Station,DefaultEdge> twoDisconnectedComponents;
+    SimpleGraph <Station,DefaultEdge> clique;
+    SimpleGraph <Station,DefaultEdge> hubAndSpoke;
+    SimpleGraph <Station,DefaultEdge> longChainOfNeighbors;
+    SimpleGraph <Station,DefaultEdge> bipartiteGraph;
+    SimpleGraph <Station,DefaultEdge> disconnectedComponents;
 
     private static SimpleGraph<Station, DefaultEdge> emptyGraph = new SimpleGraph<>(DefaultEdge.class);
 
@@ -53,65 +52,122 @@ public class ConstraintGraphNeighborhoodPresolverTest {
 
         // Completely random seed; value is not actually used since we never use any actual solvers here
         arbitrarySeed = 17;
-
-        // This mock certifier never returns SAT.
-//        IStationSubsetCertifier neverSAT = mock(IStationSubsetCertifier.class);
-//        when(neverSAT.certify(any(), any(), mockTerminationCriterion, arbitrarySeed)).thenReturn(new SolverResult(SATResult.UNSAT, 0));
-//        neverSATCertifier.add(neverSAT);
-
-        // generate graphs from files
-        noNeighbors                = new SimpleGraphBuilder(SATFCPaths.resourceLocationToPath("graphs/noNeighbors.txt")).getStationGraph();
-        bigConnectedGraph          = new SimpleGraphBuilder(SATFCPaths.resourceLocationToPath("graphs/bigConnectedGraph.txt")).getStationGraph();
-        oneRingOfNeighbors         = new SimpleGraphBuilder(SATFCPaths.resourceLocationToPath("graphs/oneRingOfNeighbors.txt")).getStationGraph();
-//        hubAndSpoke                = new SimpleGraphBuilder(SATFCPaths.resourceLocationToPath("graphs/hubAndSpoke.txt")).getStationGraph();
-//        longChainOfNeighbors       = new SimpleGraphBuilder(SATFCPaths.resourceLocationToPath("graphs/longChainOfNeighbors.txt")).getStationGraph();
-//        shortChainOfNeighbors      = new SimpleGraphBuilder(SATFCPaths.resourceLocationToPath("graphs/shortChainOfNeighbors.txt")).getStationGraph();
-//        biPartiteGraph             = new SimpleGraphBuilder(SATFCPaths.resourceLocationToPath("graphs/biPartiteGraph.txt")).getStationGraph();
-//        twoDisconnectedComponents  = new SimpleGraphBuilder(SATFCPaths.resourceLocationToPath("graphs/twoDisconnectedComponents.txt")).getStationGraph();
+        
+        generateGraphsFromFiles();
 
     }
 
-    @Test
+	@Test
     public void testEmptyGraph() throws Exception {
-    	// We expect solve to return immediately since the previous assignment will be empty.
-    	testGraph(emptyGraph, emptyGraph, Collections.emptySet(), 0, SATResult.TIMEOUT);
+    	// We expect the solver to return immediately since the previous assignment will be empty.
+    	testGraph(emptyGraph, Collections.emptySet(), 0, SATResult.TIMEOUT);
     }
     
     @Test
     public void testSingletonGraph() throws Exception {
     	SimpleGraph<Station, DefaultEdge> singleton = new SimpleGraph<>(DefaultEdge.class);
-    	singleton.addVertex(new Station(0));
-    	// We expect to return immediately since the previous assignment will be empty.
-    	testGraph(singleton, emptyGraph, Collections.singleton(new Station(0)), 0, SATResult.TIMEOUT);
+    	Station singleStation = new Station(0);
+    	singleton.addVertex(singleStation);
+    	// A single-node graph should be trivially satisfiable.
+    	testGraph(singleton, singleStation, 1, SATResult.SAT);
     }
     
     @Test
 	public void testNoNeighbors() throws Exception {
-	    testGraph(noNeighbors, emptyGraph, Collections.singleton(new Station(0)), 1, SATResult.SAT);
+    	// When the graph consists of multiple isolated nodes, we expect the neighbor search to run only one layer deep.
+	    testGraph(noNeighbors, new Station(0), 1);
+	    // We expect similar behavior even when we start from two nodes simultaneously
 	    HashSet<Station> startingStations = new HashSet<>(Arrays.asList(new Station(0), new Station(1)));
-		testGraph(noNeighbors, emptyGraph, startingStations, 1, SATResult.SAT);
+		testGraph(noNeighbors, startingStations, 1);
 	}
 
 	@Test
 	public void testBigConnectedGraph() throws Exception {
-	    testGraph(bigConnectedGraph, emptyGraph, Collections.singleton(new Station(0)), 4, SATResult.SAT);
+		// This serves as a "normal" test case, with one central starting node.
+	    testGraph(bigConnectedGraph, new Station(0), 4);
+	    // We also test the case where we start from two nodes at once.
 	    HashSet<Station> startingStations = new HashSet<>(Arrays.asList(new Station(0), new Station(14)));
-	    testGraph(bigConnectedGraph, emptyGraph, startingStations, 2, SATResult.SAT);
+	    testGraph(bigConnectedGraph, startingStations, 2);
 	}
 
 	@Test
-    public void testOneRingOfNeighbors() throws Exception {
-        testGraph(oneRingOfNeighbors, emptyGraph, Collections.singleton(new Station(0)), 1, SATResult.SAT);
-        testGraph(emptyGraph, oneRingOfNeighbors, Collections.singleton(new Station(0)), 1, SATResult.SAT);
+    public void testClique() throws Exception {
+		// Testing a fully connected graph ensures that the neighbor search excludes any nodes that have already been added.
+        testGraph(clique, new Station(0), 1);
+        // We verify that our method doesn't care about whether the graph specified is a CO or ADJ interference graph 
+        testGraph(emptyGraph, clique, Collections.singleton(new Station(0)), 1, SATResult.SAT);
     }
 	
 	@Test
-	public void testSolve() throws Exception {
+	public void testHubAndSpoke() throws Exception {
+		/*
+		 * Similar to the oneRingOfNeighbors case, except each of the peripheral nodes is connected only to the central node.
+		 * In this case we test the scenario in which each of the "peripheral" stations
+		 *  is a "new" station, and they all converge to the same neighbor. This should still run only a single iteration of 
+		 *  the neighbor search.
+		 */
+		HashSet<Station> startingStations = new HashSet<>(Arrays.asList(new Station(1), new Station(2), new Station(3),
+				new Station(4), new Station(5)));
+		testGraph(hubAndSpoke, startingStations, 1);
+		// Starting from one of the peripheral edges should give two layers of search
+		testGraph(hubAndSpoke, new Station(1), 2);
+	}
 	
+	@Test
+	public void testLongChainOfNeighbors() throws Exception {
+		// Visiting the neighbors of a long chain should be the same as visiting each member of that chain individually...
+		testGraph(longChainOfNeighbors, new Station(0), 25);
+		// ...whether we start from the front or the back.
+		testGraph(longChainOfNeighbors, new Station(25), 25);
+	}
+
+	@Test
+	public void testBipartiteGraph() throws Exception {
+		// Regardless of the station from which we start, we should only ever travel 2 layers deep
+		testGraph(bipartiteGraph, new Station(0), 2);
+		testGraph(bipartiteGraph, new Station(1), 2);
+		testGraph(bipartiteGraph, new Station(2), 2);
+		testGraph(bipartiteGraph, new Station(3), 2);
+		
+		// Starting from two nodes in the same cluster should yield the same result
+		HashSet<Station> startingStations = new HashSet<>(Arrays.asList(new Station(0), new Station(2)));
+		testGraph(bipartiteGraph, startingStations, 2);
+		
+		// Starting from two nodes in opposite clusters should reduce it to a single iteration
+		startingStations = new HashSet<>(Arrays.asList(new Station(1), new Station(2)));
+		testGraph(bipartiteGraph, startingStations, 1);
+	}
+	
+	@Test
+	public void testDisconnectedComponents() throws Exception {
+		// When starting from a single node in one of the components, the neighbor search should be unaffected by the 
+		//  other component
+		testGraph(disconnectedComponents, new Station(0), 2);
+		testGraph(disconnectedComponents, new Station(5), 3);
+		testGraph(disconnectedComponents, new Station(10), 1);
+		
+		// When starting from multiple nodes in separate components, the neighbor search should go as deep as the
+		//  component with the greatest number of neighbors
+		HashSet<Station> startingStations = new HashSet<>(Arrays.asList(new Station(0), new Station(5)));
+		testGraph(disconnectedComponents, startingStations, 3);
+		startingStations.add(new Station(10));
+		testGraph(disconnectedComponents, startingStations, 3);
+	}
+	
+	private void generateGraphsFromFiles() throws IOException, URISyntaxException
+	{
+	    noNeighbors                = new SimpleGraphBuilder(SATFCPaths.resourceLocationToPath("graphs/noNeighbors.txt")).getStationGraph();
+	    bigConnectedGraph          = new SimpleGraphBuilder(SATFCPaths.resourceLocationToPath("graphs/bigConnectedGraph.txt")).getStationGraph();
+	    clique         			   = new SimpleGraphBuilder(SATFCPaths.resourceLocationToPath("graphs/clique.txt")).getStationGraph();
+	    hubAndSpoke                = new SimpleGraphBuilder(SATFCPaths.resourceLocationToPath("graphs/hubAndSpoke.txt")).getStationGraph();
+	    longChainOfNeighbors       = new SimpleGraphBuilder(SATFCPaths.resourceLocationToPath("graphs/longChainOfNeighbors.txt")).getStationGraph();
+	    bipartiteGraph             = new SimpleGraphBuilder(SATFCPaths.resourceLocationToPath("graphs/bipartiteGraph.txt")).getStationGraph();
+	    disconnectedComponents     = new SimpleGraphBuilder(SATFCPaths.resourceLocationToPath("graphs/disconnectedComponents.txt")).getStationGraph();
 	}
 
 	private StationPackingInstance initializeInstance(SimpleGraph<Station, DefaultEdge> coGraph, 
-			SimpleGraph<Station, DefaultEdge> adjGraph, Set<Station> newStations) {
+			SimpleGraph<Station, DefaultEdge> adjGraph, Set<Station> newStations) 
+	{
 	    Set<Station> allStations = new HashSet<>(coGraph.vertexSet());
 	    allStations.addAll(adjGraph.vertexSet());
 	
@@ -134,8 +190,40 @@ public class ConstraintGraphNeighborhoodPresolverTest {
 	    return new StationPackingInstance(domains, previousAssignment);
 	}
 
+	private void testGraph(SimpleGraph<Station, DefaultEdge> graph,	Set<Station> startingStations, int numberOfTimesToCall)
+	{
+		testGraph(graph, startingStations, numberOfTimesToCall, SATResult.SAT);
+		
+	}
+
+	private void testGraph(SimpleGraph<Station, DefaultEdge> graph,
+			Station station, int numberOfTimesToCall, SATResult result)
+	{
+		testGraph(graph, Collections.singleton(station), numberOfTimesToCall, result);
+		
+	}
+
+	private void testGraph(SimpleGraph<Station, DefaultEdge> graph, Station startingStation, int numberOfTimesToCall)
+	{
+		testGraph(graph, Collections.singleton(startingStation), numberOfTimesToCall);
+	}
+	
+	private void testGraph(SimpleGraph<Station, DefaultEdge> graph, Set<Station> startingStations, 
+			int numberOfTimesToCall, SATResult expectedResult) 
+	{
+		testGraph(graph, emptyGraph, startingStations, numberOfTimesToCall, expectedResult);
+	}
+	
+	/**
+	 * @param coGraph - the graph representing interference constraints between stations on the same channel.
+	 * @param adjGraph - the graph representing interference constraints between stations on adjacent channels.
+	 * @param startingStations - the set of stations that is being "added" relative to a previous assignment.
+	 * @param expectedNumberOfLayers - the number of layers of neighbors we expect to explore; corresponds to the number
+	 * 				of times the certifier is called.
+	 * @param expectedResult - the SATResult we expect to receive from the solver.
+	 */
 	private void testGraph(SimpleGraph<Station, DefaultEdge> coGraph, SimpleGraph<Station, DefaultEdge> adjGraph, 
-			Set<Station> startingStations, int numberOfTimesToCall, SATResult expectedResult )
+			Set<Station> startingStations, int expectedNumberOfLayers, SATResult expectedResult )
 	{
 		IConstraintManager constraintManager = new GraphBackedConstraintManager(coGraph, adjGraph);
         
@@ -145,12 +233,7 @@ public class ConstraintGraphNeighborhoodPresolverTest {
 
         ConstraintGraphNeighborhoodPresolver presolver = new ConstraintGraphNeighborhoodPresolver(constraintManager, certifierList);
         SolverResult result = presolver.solve(instance, mockTerminationCriterion, arbitrarySeed);
-        assertEquals(numberOfTimesToCall, certifier.getNumberOfTimesCalled());
+        assertEquals(expectedNumberOfLayers, certifier.getNumberOfTimesCalled());
         assertEquals(expectedResult, result.getResult());
-	}
-    
-    @After
-	public void tearDown() throws Exception {
-	
 	}
 }
