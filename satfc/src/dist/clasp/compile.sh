302faf51ebf060a2c958a26c4c4488cac1ce2f65
newmanne <newman.n8@gmail.com>
2015-05-27 16:19:47 -0700

    Make compilation a bit friendlier- no relative paths in TBB flags, fail if ./configure.sh fails

--- satfc/src/dist/clasp/compile.sh
+++ satfc/src/dist/clasp/compile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#Compiles the clasp JNA library .so file for SATFC.
+# Compiles the clasp JNA library .so file for SATFC.
 
 #tar -xzvf libjnaclasp-3.tar.gz
 cd libjnaclasp-3/jna
 echo "Building clasp ..."
-./build.sh $1 $2 # These arguments are TBB_INCLUDE and TBB_LIB. If you have tbb properly installed, these arguments are not necessary
+./build.sh $1 $2 # These arguments are TBB_INCLUDE and TBB_LIB. Depending on how you have tbb installed, these arguments may not be necessary. DO NOT USE RELATIVE PATHS
 echo "Moving clasp library ..."
 mkdir -p ../../jna
 mv libjnaclasp.so ../../jna
