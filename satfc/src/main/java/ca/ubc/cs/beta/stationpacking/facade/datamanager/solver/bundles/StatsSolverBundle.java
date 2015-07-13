7742e9760ff9deb986ed49b4eb5e3f063cea68f8
newmanne <newman.n8@gmail.com>
2015-07-13 00:03:13 -0700

    Forgot to commit this file

---
+++ satfc/src/main/java/ca/ubc/cs/beta/stationpacking/facade/datamanager/solver/bundles/StatsSolverBundle.java
@@ -0,0 +1,61 @@
+package ca.ubc.cs.beta.stationpacking.facade.datamanager.solver.bundles;
+
+import ca.ubc.cs.beta.stationpacking.base.StationPackingInstance;
+import ca.ubc.cs.beta.stationpacking.cache.CacheCoordinate;
+import ca.ubc.cs.beta.stationpacking.cache.CacherProxy;
+import ca.ubc.cs.beta.stationpacking.datamanagers.constraints.IConstraintManager;
+import ca.ubc.cs.beta.stationpacking.datamanagers.stations.IStationManager;
+import ca.ubc.cs.beta.stationpacking.execution.parameters.solver.sat.ClaspLibSATSolverParameters;
+import ca.ubc.cs.beta.stationpacking.solvers.ISolver;
+import ca.ubc.cs.beta.stationpacking.solvers.VoidSolver;
+import ca.ubc.cs.beta.stationpacking.solvers.certifiers.cgneighborhood.ConstraintGraphNeighborhoodPresolver;
+import ca.ubc.cs.beta.stationpacking.solvers.certifiers.cgneighborhood.StationSubsetSATCertifier;
+import ca.ubc.cs.beta.stationpacking.solvers.componentgrouper.ConstraintGrouper;
+import ca.ubc.cs.beta.stationpacking.solvers.componentgrouper.IComponentGrouper;
+import ca.ubc.cs.beta.stationpacking.solvers.composites.SequentialSolversComposite;
+import ca.ubc.cs.beta.stationpacking.solvers.decorators.AssignmentVerifierDecorator;
+import ca.ubc.cs.beta.stationpacking.solvers.decorators.ConnectedComponentGroupingDecorator;
+import ca.ubc.cs.beta.stationpacking.solvers.decorators.UnderconstrainedStationRemoverSolverDecorator;
+import ca.ubc.cs.beta.stationpacking.solvers.decorators.cache.CacheResultDecorator;
+import ca.ubc.cs.beta.stationpacking.solvers.decorators.cache.ContainmentCacheProxy;
+import ca.ubc.cs.beta.stationpacking.solvers.decorators.cache.SubsetCacheUNSATDecorator;
+import ca.ubc.cs.beta.stationpacking.solvers.decorators.cache.SupersetCacheSATDecorator;
+import ca.ubc.cs.beta.stationpacking.solvers.sat.CompressedSATBasedSolver;
+import ca.ubc.cs.beta.stationpacking.solvers.sat.cnfencoder.SATCompressor;
+import ca.ubc.cs.beta.stationpacking.solvers.sat.solvers.AbstractCompressedSATSolver;
+import ca.ubc.cs.beta.stationpacking.solvers.sat.solvers.nonincremental.Clasp3SATSolver;
+import ca.ubc.cs.beta.stationpacking.solvers.termination.cputime.CPUTimeTerminationCriterionFactory;
+
+import java.util.Arrays;
+
+/**
+ * Created by newmanne on 2015-07-12.
+ */
+public class StatsSolverBundle extends ASolverBundle {
+
+    ISolver solver;
+
+    public StatsSolverBundle(
+            IStationManager aStationManager,
+            IConstraintManager aConstraintManager,
+            String aClaspLibraryPath
+            ) {
+        super(aStationManager, aConstraintManager);
+        solver = new VoidSolver();
+        solver = new UnderconstrainedStationRemoverSolverDecorator(solver, getConstraintManager());
+    }
+
+    @Override
+    public void close() throws Exception {
+        solver.notifyShutdown();
+    }
+
+    @Override
+    public ISolver getSolver(StationPackingInstance aInstance) {
+        return solver;
+    }
+
+}
+
+
+
