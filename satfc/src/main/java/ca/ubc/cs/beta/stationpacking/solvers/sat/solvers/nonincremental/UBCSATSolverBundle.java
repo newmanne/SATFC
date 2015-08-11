695ba3561419d30fa6e77091baa0e297e07793f3
Paul Cernek <ptcernek@gmail.com>
2015-08-11 16:52:38 -0700

    add UBCSATSolver, supporting tests, and integration

---
+++ satfc/src/main/java/ca/ubc/cs/beta/stationpacking/solvers/sat/solvers/nonincremental/UBCSATSolverBundle.java
@@ -0,0 +1,47 @@
+package ca.ubc.cs.beta.stationpacking.solvers.sat.solvers.nonincremental;
+
+import ca.ubc.cs.beta.stationpacking.base.StationPackingInstance;
+import ca.ubc.cs.beta.stationpacking.datamanagers.constraints.IConstraintManager;
+import ca.ubc.cs.beta.stationpacking.datamanagers.stations.IStationManager;
+import ca.ubc.cs.beta.stationpacking.execution.parameters.solver.sat.UBCSATLibSATSolverParameters;
+import ca.ubc.cs.beta.stationpacking.facade.SATFCFacadeBuilder;
+import ca.ubc.cs.beta.stationpacking.facade.datamanager.solver.bundles.ASolverBundle;
+import ca.ubc.cs.beta.stationpacking.solvers.ISolver;
+import ca.ubc.cs.beta.stationpacking.solvers.sat.CompressedSATBasedSolver;
+import ca.ubc.cs.beta.stationpacking.solvers.sat.cnfencoder.SATCompressor;
+import ca.ubc.cs.beta.stationpacking.solvers.sat.solvers.AbstractCompressedSATSolver;
+
+/**
+ * TODO: Add a constructor that allows the user to specify parameters other than default SATenstein.
+ * @author pcernek
+ */
+public class UBCSATSolverBundle extends ASolverBundle {
+
+    ISolver solver;
+    final String libraryPath = SATFCFacadeBuilder.findSATFCLibrary(SATFCFacadeBuilder.SATFCLibLocation.CLASP);
+
+    /**
+     * Create an abstract solver bundle with the given data management objects.
+     *
+     * @param aStationManager    - manages stations.
+     * @param aConstraintManager - manages constraints.
+     */
+    public UBCSATSolverBundle(IStationManager aStationManager, IConstraintManager aConstraintManager) {
+        super(aStationManager, aConstraintManager);
+
+        SATCompressor aCompressor = new SATCompressor(this.getConstraintManager());
+
+        AbstractCompressedSATSolver ubcsatSolver = (AbstractCompressedSATSolver) new UBCSATLibSATSolverParameters().getSATSolver();
+        solver = new CompressedSATBasedSolver(ubcsatSolver, aCompressor, aConstraintManager);
+    }
+
+    @Override
+    public ISolver getSolver(StationPackingInstance aInstance) {
+        return solver;
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+}
