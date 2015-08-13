311a15fea88750d5cdc8c4d08a9afb60ab723df5
newmanne <newman.n8@gmail.com>
2015-08-13 11:29:52 -0700

    Upgrade to clasp 3.1.3

--- satfc/src/dist/clasp/libjnaclasp-3/clasp-3/libclasp/src/weight_constraint.cpp
+++ satfc/src/dist/clasp/libjnaclasp-3/clasp-3/libclasp/src/weight_constraint.cpp
@@ -75,7 +75,6 @@ WeightLitsRep WeightLitsRep::create(Solver& s, WeightLitVec& lits, weight_t boun
 		else if (s.isTrue(x)) { bound -= lits[i].second; }
 	}
 	lits.erase(lits.begin()+j, lits.end());
-	if (bound < 0) { bound = 0; }
 	// Step 2: compute min,max, achievable weight and clear flags set in step 1
 	weight_t sumW = 0;
 	weight_t minW = MAX_W, maxW = 1;
@@ -107,8 +106,8 @@ WeightLitsRep WeightLitsRep::create(Solver& s, WeightLitVec& lits, weight_t boun
 
 // Propagates top-level assignment.
 bool WeightLitsRep::propagate(Solver& s, Literal W) {
-	if      (bound <= 0)    { return s.force(W); } // trivially SAT
-	else if (bound > reach) { return s.force(~W);} // trivially UNSAT
+	if      (sat())  { return s.force(W); } // trivially SAT
+	else if (unsat()){ return s.force(~W);} // trivially UNSAT
 	else if (s.topValue(W.var()) == value_free) { 
 		return true; 
 	}
@@ -160,12 +159,25 @@ uint32 WeightConstraint::WL::refCount() const {
 // WeightConstraint
 /////////////////////////////////////////////////////////////////////////////////////////
 WeightConstraint::CPair WeightConstraint::create(Solver& s, Literal W, WeightLitVec& lits, weight_t bound, uint32 flags) {
-	return WeightConstraint::create(s, W, WeightLitsRep::create(s, lits, bound), flags);
+	if ((flags & create_eq_bound) == 0) {
+		return WeightConstraint::create(s, W, WeightLitsRep::create(s, lits, bound), flags);
+	}
+	WeightLitsRep rep = WeightLitsRep::create(s, lits, bound + 1);
+	CPair res;
+	res.con[1] = WeightConstraint::createImpl(s, ~W, rep, flags);
+	if (res.ok()) {
+		rep.bound -= 1;
+		// redo coefficient reduction
+		for (unsigned i = 0; i != rep.size && rep.lits[i].second > rep.bound; ++i) {
+			rep.reach -= rep.lits[i].second;
+			rep.reach += (rep.lits[i].second = rep.bound);
+		}
+		res.con[0] = WeightConstraint::createImpl(s, W, rep, flags);
+	}
+	return res;	
 }
 WeightConstraint::CPair WeightConstraint::create(Solver& s, Literal W, WeightLitsRep rep, uint32 flags) {
 	CPair res;
-	if (!s.sharedContext()->physicalShareProblem()) { flags |= create_no_share; }
-	if (s.sharedContext()->frozen())                { flags |= (create_no_freeze|create_no_share); }
 	res.con[0] = createImpl(s, W, rep, flags);
 	if ((flags & create_eq_bound) != 0 && res.ok()) {
 		// Adds constraints for W == (lits == bound)
@@ -203,6 +215,8 @@ WeightConstraint* WeightConstraint::createImpl(Solver& s, Literal W, WeightLitsR
 		act  = ((flags & (create_only_btb|create_only_bfb)) / create_only_btb) - 1u;
 		act += (act == 2u);
 	}
+	if (!s.sharedContext()->physicalShareProblem()) { flags |= create_no_share; }
+	if (s.sharedContext()->frozen())                { flags |= (create_no_freeze|create_no_share); }
 	bool   hasW = rep.hasWeights();
 	uint32 size = 1 + rep.size;
 	uint32 nb   = sizeof(WeightConstraint) + (size+uint32(hasW))*sizeof(UndoInfo);
--- satfc/src/dist/clasp/libjnaclasp-3/clasp-3/libclasp/tests/program_builder_test.cpp
+++ satfc/src/dist/clasp/libjnaclasp-3/clasp-3/libclasp/tests/program_builder_test.cpp
@@ -599,8 +599,8 @@ public:
 		;
 		// Program is unsat because b must be true and false at the same time.
 		CPPUNIT_ASSERT_EQUAL(true, builder.endProgram() && ctx.endInit());
-		CPPUNIT_ASSERT(builder.getAtom(2)->literal() == posLit(0));
-		CPPUNIT_ASSERT(builder.getAtom(3)->literal() == negLit(0));
+		CPPUNIT_ASSERT(builder.getLiteral(2) == posLit(0));
+		CPPUNIT_ASSERT(builder.getLiteral(3) == negLit(0));
 		CPPUNIT_ASSERT(ctx.numVars() == 0);
 	}
 
@@ -615,9 +615,9 @@ public:
 		;
 		// Program is unsat because b must be true and false at the same time.
 		CPPUNIT_ASSERT_EQUAL(true, builder.endProgram() && ctx.endInit());
-		CPPUNIT_ASSERT(builder.getAtom(2)->literal() == posLit(0));
-		CPPUNIT_ASSERT(builder.getAtom(3)->literal() == negLit(0));
-		CPPUNIT_ASSERT(builder.getAtom(4)->literal() == negLit(0));
+		CPPUNIT_ASSERT(builder.getLiteral(2) == posLit(0));
+		CPPUNIT_ASSERT(builder.getLiteral(3) == negLit(0));
+		CPPUNIT_ASSERT(builder.getLiteral(4) == negLit(0));
 		CPPUNIT_ASSERT(ctx.numVars() == 0);
 	}
 	void testFuzzBug() {
@@ -2487,6 +2487,9 @@ class PBBuilderTest : public CppUnit::TestFixture {
 	CPPUNIT_TEST(testProduct);
 	CPPUNIT_TEST(testProductTrue);
 	CPPUNIT_TEST(testProductFalse);
+	CPPUNIT_TEST(testInconsistent);
+	CPPUNIT_TEST(testInconsistentW);
+	CPPUNIT_TEST(testCoefficientReductionOnEq);
 	CPPUNIT_TEST_SUITE_END();
 public:
 	void setUp() {
@@ -2544,6 +2547,36 @@ public:
 		ctx.master()->force(negLit(2), 0);
 		Literal x = builder.addProduct(p1);
 		CPPUNIT_ASSERT(x == negLit(0));
+	}
+	void testInconsistent() {
+		builder.prepareProblem(1, 0, 0);
+		WeightLitVec con;
+		con.push_back(WeightLiteral(posLit(1), 1));
+		CPPUNIT_ASSERT_EQUAL(true, builder.addConstraint(con, 1));
+		con.assign(1, WeightLiteral(posLit(1), 1));
+		CPPUNIT_ASSERT_EQUAL(false, builder.addConstraint(con, 0, true));
+		CPPUNIT_ASSERT_EQUAL(false, builder.endProgram());
+	}
+	void testInconsistentW() {
+		builder.prepareProblem(2, 0, 0);
+		WeightLitVec con;
+		con.push_back(WeightLiteral(posLit(1), 1));
+		CPPUNIT_ASSERT_EQUAL(true, builder.addConstraint(con, 1));
+		con.assign(1, WeightLiteral(posLit(1), 3));
+		con.push_back(WeightLiteral(posLit(2), 2));
+		CPPUNIT_ASSERT_EQUAL(false, builder.addConstraint(con, 2, true));
+		CPPUNIT_ASSERT_EQUAL(false, builder.endProgram());
+	}
+	void testCoefficientReductionOnEq() {
+		builder.prepareProblem(4, 0, 0);
+		WeightLitVec con;
+		con.push_back(WeightLiteral(posLit(1), 3));
+		con.push_back(WeightLiteral(posLit(2), 2));
+		con.push_back(WeightLiteral(posLit(3), 1));
+		con.push_back(WeightLiteral(posLit(4), 1));
+		CPPUNIT_ASSERT_EQUAL(true, builder.addConstraint(con, 2, true));
+		CPPUNIT_ASSERT_EQUAL(true, builder.endProgram());
+		CPPUNIT_ASSERT(ctx.master()->isFalse(posLit(1)));
 	}
 private:
 	SharedContext ctx;
