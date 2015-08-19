3e60b7cff18f164f2e2129bdd5e971929a8999c4
newmanne <newman.n8@gmail.com>
2015-08-18 17:12:25 -0700

    Use variable in logback.groovy

--- satfc/src/main/resources/logback.groovy
+++ satfc/src/main/resources/logback.groovy
@@ -4,16 +4,18 @@ def ROOT_LOG_LEVEL_PROPERTY = System.getProperty("SATFC.root.log.level")
 def ROOT_LOG_LEVEL = ROOT_LOG_LEVEL_PROPERTY == null ? Level.INFO : Level.valueOf(ROOT_LOG_LEVEL_PROPERTY)
 println "Setting root log level to $ROOT_LOG_LEVEL"
 
+def LOG_FORMAT = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{0} - %msg%n"
+
 appender("FILE", FileAppender) {
     file = "SATFC.log"
     encoder(PatternLayoutEncoder) {
-        pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{0} - %msg%n"
+        pattern = LOG_FORMAT
     }
 }
 
 appender("CONSOLE", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
-        pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{0} - %msg%n"
+        pattern = LOG_FORMAT
     }
 }
 
